fb6994f9b03be259b02b8df1789f1df03a5251ac
Max Kramer <max.kramer@kit.edu>
2014-01-13 16:39:37 +0000

    test workspace

--- edu.kit.ipd.sdq.vitruvius.tests.mockup/src/edu/kit/ipd/sdq/vitruvius/tests/mockup/MetaRepositoryTest.java
+++ edu.kit.ipd.sdq.vitruvius.tests.mockup/src/edu/kit/ipd/sdq/vitruvius/tests/mockup/MetaRepositoryTest.java
@@ -1,27 +1,52 @@
 package edu.kit.ipd.sdq.vitruvius.tests.mockup;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
-
-import java.util.Collection;
-import java.util.Collections;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes.Mapping;
 import edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes.Metamodel;
-import edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes.URI;
-import edu.kit.ipd.sdq.vitruvius.framework.metarepository.*;
+import edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes.VURI;
+import edu.kit.ipd.sdq.vitruvius.framework.metarepository.MetaRepositoryImpl;
 
 public class MetaRepositoryTest {
-
+	
 	@Test
-	public void test() {
-		MetaRepositoryImpl metaRepository = new MetaRepositoryImpl();
-		Metamodel mm1 = new Metamodel(new URI("aproject/metamodels/mm1.ecore"));
-		metaRepository.addMetamodel(mm1);
-		Metamodel mm2 = new Metamodel(new URI("aproject/metamodels/mm2.ecore"));
-		metaRepository.addMetamodel(mm1);
+	public void testAll() {
+		MetaRepositoryImpl metaRepository = testMetaRepository();
+		
+		testAddMapping(metaRepository, "aproject/metamodels/mm1.ecore", "aproject/metamodels/mm2.ecore");
+		
+		
+		// generiere VSUM plugins (jetzt erst mal hart verdrahtet)
+		
 		// TODO KEEP ON WORKING HERE
 	}
 
+	public MetaRepositoryImpl testMetaRepository() {
+		MetaRepositoryImpl metaRepository = new MetaRepositoryImpl();
+		assertNotNull(metaRepository);
+		return metaRepository;
+	}
+	
+	public void testAddMapping(MetaRepositoryImpl metaRepository, String uri1String, String uri2String) {
+		VURI uri1 = new VURI(uri1String);
+		testAddMetamodel(metaRepository, uri1);
+		
+		VURI uri2 = new VURI(uri2String);
+		testAddMetamodel(metaRepository, uri2);
+		
+		Mapping mapping = new Mapping(uri1, uri2);
+		metaRepository.addMapping(mapping);
+		assertTrue(true);
+	}
+
+	public void testAddMetamodel(MetaRepositoryImpl metaRepository, VURI uri) {
+		Metamodel mm1 = new Metamodel(uri);
+		metaRepository.addMetamodel(mm1);
+		
+		assertEquals(metaRepository.getMetamodel(uri), mm1);
+	}
 }
