9a3651fb09181b5c16d35472e1b3fe93656e3c47
Michael Langhammer <langhammer@kit.edu>
2014-01-13 16:53:22 +0000

    create and export class ViewTypeManagerImpl

---
+++ edu.kit.ipd.sdq.vitruvius.framework.design.vts/src/edu/kit/ipd/sdq/vitruvius/framework/design/viewtype/manager/ViewTypeManagerImpl.java
@@ -0,0 +1,26 @@
+package edu.kit.ipd.sdq.vitruvius.framework.design.viewtype.manager;
+
+
+import edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes.VURI;
+import edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes.ViewType;
+import edu.kit.ipd.sdq.vitruvius.framework.contracts.interfaces.ViewTypeManaging;
+
+public class ViewTypeManagerImpl implements ViewTypeManaging {
+
+    public ViewTypeManagerImpl() {
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void addViewType(ViewType viewType) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public ViewType getViewType(VURI uri) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
