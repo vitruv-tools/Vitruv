0ffc11749793b77b11e13324a5883c8d0cfc60f4
Max Kramer <max.kramer@kit.edu>
2014-01-20 16:25:38 +0000

    project specific cleanup and formatter settings for all projects

--- edu.kit.ipd.sdq.vitruvius.tests.mockup/src/edu/kit/ipd/sdq/vitruvius/tests/mockup/MetaRepositoryTest.java
+++ edu.kit.ipd.sdq.vitruvius.tests.mockup/src/edu/kit/ipd/sdq/vitruvius/tests/mockup/MetaRepositoryTest.java
@@ -17,126 +17,120 @@ import edu.kit.ipd.sdq.vitruvius.framework.metarepository.MetaRepositoryImpl;
 
 public class MetaRepositoryTest {
 
-	@Test
-	public void testAll() {
+    @Test
+    public void testAll() {
 
-		testAddMapping("MockupProject/metamodels/pcm_mockup.ecore",
-				"MockupProject/metamodels/uml_mockup.ecore");
+        testAddMapping("MockupProject/metamodels/pcm_mockup.ecore", "MockupProject/metamodels/uml_mockup.ecore");
 
-		testAddViewType("MockupProject/metamodels/pcm_mockup.ecore",
-				"MockupProject/metamodels/uml_mockup.ecore",
-				" MockupProject/viewtypes/pcm__uml_mockup.ecore");
+        testAddViewType("MockupProject/metamodels/pcm_mockup.ecore", "MockupProject/metamodels/uml_mockup.ecore",
+                " MockupProject/viewtypes/pcm__uml_mockup.ecore");
 
-		testGetProjectInput("MockupProject/metamodels/pcm_mockup.ecore",
-				"MockupProject/metamodels/uml_mockup.ecore",
-				" MockupProject/viewtypes/pcm__uml_mockup.ecore");
-		
-		// generiere VSUM plugins (jetzt erst mal hart verdrahtet)
+        testGetProjectInput("MockupProject/metamodels/pcm_mockup.ecore", "MockupProject/metamodels/uml_mockup.ecore",
+                " MockupProject/viewtypes/pcm__uml_mockup.ecore");
 
-		// TODO KEEP ON WORKING HERE
-	}
+        // generiere VSUM plugins (jetzt erst mal hart verdrahtet)
 
-	public MetaRepositoryImpl testMetaRepository() {
-		MetaRepositoryImpl metaRepository = new MetaRepositoryImpl();
-		assertNotNull(metaRepository);
-		return metaRepository;
-	}
+        // TODO KEEP ON WORKING HERE
+    }
 
-	public void testAddMetamodel(MetaRepositoryImpl metaRepository, VURI uri) {
-		Metamodel mm1 = new Metamodel(uri);
-		metaRepository.addMetamodel(mm1);
+    public MetaRepositoryImpl testMetaRepository() {
+        MetaRepositoryImpl metaRepository = new MetaRepositoryImpl();
+        assertNotNull(metaRepository);
+        return metaRepository;
+    }
 
-		assertEquals(metaRepository.getMetamodel(uri), mm1);
-	}
+    public void testAddMetamodel(final MetaRepositoryImpl metaRepository, final VURI uri) {
+        Metamodel mm1 = new Metamodel(uri);
+        metaRepository.addMetamodel(mm1);
 
-	public void testAddMapping(String uri1String, String uri2String) {
-		MetaRepositoryImpl metaRepository = testMetaRepository();
-		testAddMapping(metaRepository, uri1String, uri2String);
-	}
+        assertEquals(metaRepository.getMetamodel(uri), mm1);
+    }
 
-	public Pair<VURI, VURI> testAddMapping(MetaRepositoryImpl metaRepository,
-			String uri1String, String uri2String) {
-		VURI uri1 = new VURI(uri1String);
-		testAddMetamodel(metaRepository, uri1);
+    public void testAddMapping(final String uri1String, final String uri2String) {
+        MetaRepositoryImpl metaRepository = testMetaRepository();
+        testAddMapping(metaRepository, uri1String, uri2String);
+    }
 
-		VURI uri2 = new VURI(uri2String);
-		testAddMetamodel(metaRepository, uri2);
+    public Pair<VURI, VURI> testAddMapping(final MetaRepositoryImpl metaRepository, final String uri1String,
+            final String uri2String) {
+        VURI uri1 = new VURI(uri1String);
+        testAddMetamodel(metaRepository, uri1);
 
-		Mapping mapping = new Mapping(uri1, uri2);
-		metaRepository.addMapping(mapping);
+        VURI uri2 = new VURI(uri2String);
+        testAddMetamodel(metaRepository, uri2);
 
-		Mapping mapping2 = metaRepository.getMapping(uri1, uri2);
-		assertEquals(mapping, mapping2);
+        Mapping mapping = new Mapping(uri1, uri2);
+        metaRepository.addMapping(mapping);
 
-		return new Pair<VURI, VURI>(uri1, uri2);
-	}
+        Mapping mapping2 = metaRepository.getMapping(uri1, uri2);
+        assertEquals(mapping, mapping2);
 
-	public void testAddViewType(String uri1String, String uri2String,
-			String viewTypeURIString) {
-		MetaRepositoryImpl metaRepository = testMetaRepository();
+        return new Pair<VURI, VURI>(uri1, uri2);
+    }
 
-		VURI uri1 = new VURI(uri1String);
-		testAddMetamodel(metaRepository, uri1);
+    public void testAddViewType(final String uri1String, final String uri2String, final String viewTypeURIString) {
+        MetaRepositoryImpl metaRepository = testMetaRepository();
 
-		VURI uri2 = new VURI(uri2String);
-		testAddMetamodel(metaRepository, uri2);
+        VURI uri1 = new VURI(uri1String);
+        testAddMetamodel(metaRepository, uri1);
 
-		testAddViewType(metaRepository, uri1, uri2, viewTypeURIString);
-	}
+        VURI uri2 = new VURI(uri2String);
+        testAddMetamodel(metaRepository, uri2);
 
-	public VURI testAddViewType(MetaRepositoryImpl metaRepository, VURI uri1,
-			VURI uri2, String viewTypeURIString) {
+        testAddViewType(metaRepository, uri1, uri2, viewTypeURIString);
+    }
 
-		VURI viewTypeURI = new VURI(viewTypeURIString);
-		ViewType vt = new ViewType(viewTypeURI, uri1, uri2);
-		metaRepository.addViewType(vt);
+    public VURI testAddViewType(final MetaRepositoryImpl metaRepository, final VURI uri1, final VURI uri2,
+            final String viewTypeURIString) {
 
-		ViewType vt2 = metaRepository.getViewType(viewTypeURI);
-		assertEquals(vt, vt2);
-		
-		return viewTypeURI;
-	}
+        VURI viewTypeURI = new VURI(viewTypeURIString);
+        ViewType vt = new ViewType(viewTypeURI, uri1, uri2);
+        metaRepository.addViewType(vt);
 
-	private void testGetProjectInput(String uri1String, String uri2String,
-			String viewTypeURIString) {
-		MetaRepositoryImpl metaRepository = testMetaRepository();
+        ViewType vt2 = metaRepository.getViewType(viewTypeURI);
+        assertEquals(vt, vt2);
 
-		Pair<VURI, VURI> mmURIs = testAddMapping(metaRepository, uri1String,
-				uri2String);
+        return viewTypeURI;
+    }
 
-		VURI uri1 = mmURIs.getFirst();
-		VURI uri2 = mmURIs.getSecond();
+    private void testGetProjectInput(final String uri1String, final String uri2String, final String viewTypeURIString) {
+        MetaRepositoryImpl metaRepository = testMetaRepository();
 
-		VURI vtURI = testAddViewType(metaRepository, uri1, uri2, viewTypeURIString);
-		
-		ProjectInput projectInput = metaRepository.getProjectInput();
-		assertMetamodels(uri1, metaRepository, projectInput);
-		assertMetamodels(uri2, metaRepository, projectInput);
-		assertViewType(vtURI, metaRepository, projectInput);
-		assertMapping(uri1, uri2, metaRepository, projectInput);
-	}
+        Pair<VURI, VURI> mmURIs = testAddMapping(metaRepository, uri1String, uri2String);
 
-	private void assertMetamodels(VURI uri1, MetaRepositoryImpl metaRepository,
-			ProjectInput projectInput) {
-		ClaimableMap<VURI, Metamodel> uri2MetamodelMap = projectInput.getUri2MetamodelMap();
-		Metamodel mm11 = uri2MetamodelMap.get(uri1);
-		Metamodel mm12 = metaRepository.getMetamodel(uri1);
-		assertEquals(mm11, mm12);
-	}
+        VURI uri1 = mmURIs.getFirst();
+        VURI uri2 = mmURIs.getSecond();
 
-	private void assertViewType(VURI vtURI, MetaRepositoryImpl metaRepository,
-			ProjectInput projectInput) {
-		ClaimableMap<VURI, ViewType> uri2ViewTypeMap = projectInput.getUri2ViewTypeMap();
-		ViewType viewType1 = uri2ViewTypeMap.get(vtURI);
-		ViewType viewType2 = metaRepository.getViewType(vtURI);
-		assertEquals(viewType1, viewType2);
-	}
+        VURI vtURI = testAddViewType(metaRepository, uri1, uri2, viewTypeURIString);
 
-	private void assertMapping(VURI uri1, VURI uri2,
-			MetaRepositoryImpl metaRepository, ProjectInput projectInput) {
-		ClaimableConcatMap<VURI, Mapping> uris2MappingMap = projectInput.getUris2MappingMap();
-		Mapping mapping1 = uris2MappingMap.get(uri1, uri2);
-		Mapping mapping2 = metaRepository.getMapping(uri1, uri2);
-		assertEquals(mapping1, mapping2);
-	}
+        ProjectInput projectInput = metaRepository.getProjectInput();
+        assertMetamodels(uri1, metaRepository, projectInput);
+        assertMetamodels(uri2, metaRepository, projectInput);
+        assertViewType(vtURI, metaRepository, projectInput);
+        assertMapping(uri1, uri2, metaRepository, projectInput);
+    }
+
+    private void assertMetamodels(final VURI uri1, final MetaRepositoryImpl metaRepository,
+            final ProjectInput projectInput) {
+        ClaimableMap<VURI, Metamodel> uri2MetamodelMap = projectInput.getUri2MetamodelMap();
+        Metamodel mm11 = uri2MetamodelMap.get(uri1);
+        Metamodel mm12 = metaRepository.getMetamodel(uri1);
+        assertEquals(mm11, mm12);
+    }
+
+    private void assertViewType(final VURI vtURI, final MetaRepositoryImpl metaRepository,
+            final ProjectInput projectInput) {
+        ClaimableMap<VURI, ViewType> uri2ViewTypeMap = projectInput.getUri2ViewTypeMap();
+        ViewType viewType1 = uri2ViewTypeMap.get(vtURI);
+        ViewType viewType2 = metaRepository.getViewType(vtURI);
+        assertEquals(viewType1, viewType2);
+    }
+
+    private void assertMapping(final VURI uri1, final VURI uri2, final MetaRepositoryImpl metaRepository,
+            final ProjectInput projectInput) {
+        ClaimableConcatMap<VURI, Mapping> uris2MappingMap = projectInput.getUris2MappingMap();
+        Mapping mapping1 = uris2MappingMap.get(uri1, uri2);
+        Mapping mapping2 = metaRepository.getMapping(uri1, uri2);
+        assertEquals(mapping1, mapping2);
+    }
 }
