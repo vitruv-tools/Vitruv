3f6958fa68e0706051f15abd90c80201bb381938
Max Kramer <max.kramer@kit.edu>
2014-02-13 12:41:36 +0000

    getcorr

--- edu.kit.ipd.sdq.vitruvius.framework.architecturalcontracts/src/edu/kit/ipd/sdq/vitruvius/framework/contracts/datatypes/CorrespondenceInstance.java
+++ edu.kit.ipd.sdq.vitruvius.framework.architecturalcontracts/src/edu/kit/ipd/sdq/vitruvius/framework/contracts/datatypes/CorrespondenceInstance.java
@@ -1,9 +1,17 @@
 package edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes;
 
+import java.util.Collection;
+
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
+
+import edu.kit.ipd.sdq.vitruvius.framework.meta.correspondence.Correspondence;
+import edu.kit.ipd.sdq.vitruvius.framework.meta.correspondence.CorrespondenceFactory;
+import edu.kit.ipd.sdq.vitruvius.framework.meta.correspondence.Correspondences;
 
 public class CorrespondenceInstance extends ModelInstance {
     private Mapping mapping;
+    private Correspondences correspondences;
 
     public CorrespondenceInstance(final Mapping mapping, final VURI vuri, final Resource resource) {
         super(vuri, resource);
@@ -11,9 +19,19 @@ public class CorrespondenceInstance extends ModelInstance {
         // FIXME AAAA KEEP ON WORKING HERE
         // create model instance of the correspondence instance metamodel
         // link to this instance from a private field
+        this.correspondences = CorrespondenceFactory.eINSTANCE.createCorrespondences();
     }
 
     public Mapping getMapping() {
         return this.mapping;
+    }
+
+    public Collection<Correspondence> getAllCorrespondences(final EObject eObject) {
+        // this.correspondences.getCorrespondences().
+        return null;
+    }
+
+    public void addCorrespondence(final Correspondence correspondence) {
+        // TODO
     }
 }
