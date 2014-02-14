fae5ed0d462ea2439d1cd675cdeaedd11653d1bb
Michael Langhammer <langhammer@kit.edu>
2014-02-14 14:18:24 +0000

    getSyncTransformation is now deprecated

--- edu.kit.ipd.sdq.vitruvius.framework.architecturalcontracts/src/edu/kit/ipd/sdq/vitruvius/framework/contracts/interfaces/TransformationExecutingProviding.java
+++ edu.kit.ipd.sdq.vitruvius.framework.architecturalcontracts/src/edu/kit/ipd/sdq/vitruvius/framework/contracts/interfaces/TransformationExecutingProviding.java
@@ -5,6 +5,8 @@ import edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes.SyncTransformatio
 import edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes.VURI;
 
 public interface TransformationExecutingProviding {
+    // TODO: check wheather we need the method getSyncTransformation
+    @Deprecated
     public SyncTransformation getSyncTransformation(VURI mmURI1, Change change, VURI mmURI2);
 
     public TransformationExecuting getTransformationExecuting(VURI mmURI1, VURI mmURI2);
