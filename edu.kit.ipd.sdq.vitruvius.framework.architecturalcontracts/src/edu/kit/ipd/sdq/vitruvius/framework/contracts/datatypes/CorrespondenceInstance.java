132dd303cb6dda1411f86b6563f31626bb601570
Max Kramer <max.kramer@kit.edu>
2014-02-14 11:37:09 +0000

    correspondenceinstance implemented

--- edu.kit.ipd.sdq.vitruvius.framework.architecturalcontracts/src/edu/kit/ipd/sdq/vitruvius/framework/contracts/datatypes/CorrespondenceInstance.java
+++ edu.kit.ipd.sdq.vitruvius.framework.architecturalcontracts/src/edu/kit/ipd/sdq/vitruvius/framework/contracts/datatypes/CorrespondenceInstance.java
@@ -1,7 +1,11 @@
 package edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes;
 
-import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 
@@ -12,26 +16,39 @@ import edu.kit.ipd.sdq.vitruvius.framework.meta.correspondence.Correspondences;
 public class CorrespondenceInstance extends ModelInstance {
     private Mapping mapping;
     private Correspondences correspondences;
+    private Map<EObject, Set<Correspondence>> eObject2CorrespondencesMap;
 
     public CorrespondenceInstance(final Mapping mapping, final VURI vuri, final Resource resource) {
         super(vuri, resource);
         this.mapping = mapping;
-        // FIXME AAAA KEEP ON WORKING HERE
-        // create model instance of the correspondence instance metamodel
-        // link to this instance from a private field
         this.correspondences = CorrespondenceFactory.eINSTANCE.createCorrespondences();
+        // TODO implement lazy loading for correspondences because they may get really big
+        this.eObject2CorrespondencesMap = new HashMap<EObject, Set<Correspondence>>();
     }
 
     public Mapping getMapping() {
         return this.mapping;
     }
 
-    public Collection<Correspondence> getAllCorrespondences(final EObject eObject) {
-        // this.correspondences.getCorrespondences().
-        return null;
+    public Set<Correspondence> getAllCorrespondences(final EObject eObject) {
+        return this.eObject2CorrespondencesMap.get(eObject);
     }
 
     public void addCorrespondence(final Correspondence correspondence) {
-        // TODO
+        // add correspondence to model
+        this.correspondences.getCorrespondences().add(correspondence);
+        EList<EObject> allInvolvedEObjects = correspondence.getAllInvolvedEObjects();
+        // add all involved eObjects to the sets for these objects in the map
+        for (EObject involedEObject : allInvolvedEObjects) {
+            Set<Correspondence> correspondences = this.eObject2CorrespondencesMap.get(involedEObject);
+            if (correspondences == null) {
+                correspondences = new HashSet<Correspondence>();
+                this.eObject2CorrespondencesMap.put(involedEObject, correspondences);
+            }
+            if (!correspondences.contains(correspondence)) {
+                correspondences.add(correspondence);
+            }
+        }
+
     }
 }
