45b3889195e97267d0b4608390e2a614f9ca1b42
Michael Langhammer <langhammer@kit.edu>
2014-03-26 07:22:23 +0000

    generated from new genmodel

--- edu.kit.ipd.sdq.vitruvius.framework.meta.correspondence.edit/src/edu/kit/ipd/sdq/vitruvius/framework/meta/correspondence/provider/CorrespondencesItemProvider.java
+++ edu.kit.ipd.sdq.vitruvius.framework.meta.correspondence.edit/src/edu/kit/ipd/sdq/vitruvius/framework/meta/correspondence/provider/CorrespondencesItemProvider.java
@@ -3,8 +3,10 @@
 package edu.kit.ipd.sdq.vitruvius.framework.meta.correspondence.provider;
 
 
+import edu.kit.ipd.sdq.vitruvius.framework.meta.correspondence.CorrespondenceFactory;
 import edu.kit.ipd.sdq.vitruvius.framework.meta.correspondence.CorrespondencePackage;
 
+import edu.kit.ipd.sdq.vitruvius.framework.meta.correspondence.Correspondences;
 import java.util.Collection;
 import java.util.List;
 
@@ -13,6 +15,7 @@ import org.eclipse.emf.common.notify.Notification;
 
 import org.eclipse.emf.common.util.ResourceLocator;
 
+import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
 import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
 import org.eclipse.emf.edit.provider.IItemLabelProvider;
@@ -21,6 +24,7 @@ import org.eclipse.emf.edit.provider.IItemPropertySource;
 import org.eclipse.emf.edit.provider.IStructuredItemContentProvider;
 import org.eclipse.emf.edit.provider.ITreeItemContentProvider;
 import org.eclipse.emf.edit.provider.ItemProviderAdapter;
+import org.eclipse.emf.edit.provider.ViewerNotification;
 
 /**
  * This is the item provider adapter for a {@link edu.kit.ipd.sdq.vitruvius.framework.meta.correspondence.Correspondences} object.
@@ -85,6 +89,36 @@ public class CorrespondencesItemProvider
     }
 
     /**
+     * This specifies how to implement {@link #getChildren} and is used to deduce an appropriate feature for an
+     * {@link org.eclipse.emf.edit.command.AddCommand}, {@link org.eclipse.emf.edit.command.RemoveCommand} or
+     * {@link org.eclipse.emf.edit.command.MoveCommand} in {@link #createCommand}.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    @Override
+    public Collection<? extends EStructuralFeature> getChildrenFeatures(Object object) {
+        if (childrenFeatures == null) {
+            super.getChildrenFeatures(object);
+            childrenFeatures.add(CorrespondencePackage.Literals.CORRESPONDENCES__CORRESPONDENCES);
+        }
+        return childrenFeatures;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    @Override
+    protected EStructuralFeature getChildFeature(Object object, Object child) {
+        // Check the type of the specified child object and return the proper feature to use for
+        // adding (see {@link AddCommand}) it as a child.
+
+        return super.getChildFeature(object, child);
+    }
+
+    /**
      * This returns Correspondences.gif.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
@@ -116,6 +150,12 @@ public class CorrespondencesItemProvider
     @Override
     public void notifyChanged(Notification notification) {
         updateChildren(notification);
+
+        switch (notification.getFeatureID(Correspondences.class)) {
+            case CorrespondencePackage.CORRESPONDENCES__CORRESPONDENCES:
+                fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), true, false));
+                return;
+        }
         super.notifyChanged(notification);
     }
 
@@ -129,6 +169,31 @@ public class CorrespondencesItemProvider
     @Override
     protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object) {
         super.collectNewChildDescriptors(newChildDescriptors, object);
+
+        newChildDescriptors.add
+            (createChildParameter
+                (CorrespondencePackage.Literals.CORRESPONDENCES__CORRESPONDENCES,
+                 CorrespondenceFactory.eINSTANCE.createEObjectCorrespondence()));
+
+        newChildDescriptors.add
+            (createChildParameter
+                (CorrespondencePackage.Literals.CORRESPONDENCES__CORRESPONDENCES,
+                 CorrespondenceFactory.eINSTANCE.createEAttributeCorrespondence()));
+
+        newChildDescriptors.add
+            (createChildParameter
+                (CorrespondencePackage.Literals.CORRESPONDENCES__CORRESPONDENCES,
+                 CorrespondenceFactory.eINSTANCE.createEContainmentReferenceCorrespondence()));
+
+        newChildDescriptors.add
+            (createChildParameter
+                (CorrespondencePackage.Literals.CORRESPONDENCES__CORRESPONDENCES,
+                 CorrespondenceFactory.eINSTANCE.createPartialEAttributeCorrespondence()));
+
+        newChildDescriptors.add
+            (createChildParameter
+                (CorrespondencePackage.Literals.CORRESPONDENCES__CORRESPONDENCES,
+                 CorrespondenceFactory.eINSTANCE.createPartialEReferenceCorrespondence()));
     }
 
     /**
