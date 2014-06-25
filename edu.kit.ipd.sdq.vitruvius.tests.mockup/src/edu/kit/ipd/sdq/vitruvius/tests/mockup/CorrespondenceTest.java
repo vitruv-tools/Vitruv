4c9ea505c250c8294ec46cdb31538c7adc5c878e
Max Kramer <max.kramer@kit.edu>
2014-06-25 12:30:56 +0000

    initial correspondence test that makes some sense

--- edu.kit.ipd.sdq.vitruvius.tests.mockup/src/edu/kit/ipd/sdq/vitruvius/tests/mockup/CorrespondenceTest.java
+++ edu.kit.ipd.sdq.vitruvius.tests.mockup/src/edu/kit/ipd/sdq/vitruvius/tests/mockup/CorrespondenceTest.java
@@ -1,7 +1,19 @@
 package edu.kit.ipd.sdq.vitruvius.tests.mockup;
 
+import static org.junit.Assert.assertTrue;
+
+import java.util.Set;
+
 import org.junit.Test;
 
+import pcm_mockup.Repository;
+import uml_mockup.UPackage;
+import edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes.CorrespondenceInstance;
+import edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes.ModelInstance;
+import edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes.VURI;
+import edu.kit.ipd.sdq.vitruvius.framework.meta.correspondence.Correspondence;
+import edu.kit.ipd.sdq.vitruvius.framework.meta.correspondence.CorrespondenceFactory;
+import edu.kit.ipd.sdq.vitruvius.framework.meta.correspondence.EObjectCorrespondence;
 import edu.kit.ipd.sdq.vitruvius.framework.vsum.VSUMImpl;
 
 public class CorrespondenceTest extends VSUMTest {
@@ -9,6 +21,20 @@ public class CorrespondenceTest extends VSUMTest {
     @Test
     public void testAll() {
         VSUMImpl vsum = testMetaRepositoryVSUMAndModelInstancesCreation();
-        // FIXME AA KEEP ON WORKING HERE
+        VURI pcmVURI = VURI.getInstance(PCM_INSTANCE_URI);
+        VURI umlVURI = VURI.getInstance(UML_INSTANCE_URI);
+        ModelInstance pcmInstance = vsum.getModelInstanceOriginal(pcmVURI);
+        ModelInstance umlInstance = vsum.getModelInstanceOriginal(umlVURI);
+        Repository repo = pcmInstance.getUniqueRootEObjectIfCorrectlyTyped(Repository.class);
+        UPackage pkg = umlInstance.getUniqueRootEObjectIfCorrectlyTyped(UPackage.class);
+        VURI pcmMMVURI = VURI.getInstance(PCM_MM_URI);
+        VURI umlMMVURI = VURI.getInstance(UML_MM_URI);
+        CorrespondenceInstance corresp = vsum.getCorrespondenceInstanceOriginal(pcmMMVURI, umlMMVURI);
+        EObjectCorrespondence repo2pkg = CorrespondenceFactory.eINSTANCE.createEObjectCorrespondence();
+        repo2pkg.setElementA(repo);
+        repo2pkg.setElementB(pkg);
+        corresp.addSameTypeCorrespondence(repo2pkg);
+        Set<Correspondence> allRepoCorresp = corresp.getAllCorrespondences(repo);
+        assertTrue(allRepoCorresp.contains(repo2pkg));
     }
 }
