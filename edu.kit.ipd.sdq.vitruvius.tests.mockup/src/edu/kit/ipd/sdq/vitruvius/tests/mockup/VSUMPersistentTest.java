6cc4f676831593e393e0dd3de31fda3ea48a5d2e
Michael Langhammer <langhammer@kit.edu>
2014-06-25 09:17:54 +0000

    adapt persistent test to  VURI

--- edu.kit.ipd.sdq.vitruvius.tests.mockup/src/edu/kit/ipd/sdq/vitruvius/tests/mockup/VSUMPersistentTest.java
+++ edu.kit.ipd.sdq.vitruvius.tests.mockup/src/edu/kit/ipd/sdq/vitruvius/tests/mockup/VSUMPersistentTest.java
@@ -1,5 +1,7 @@
 package edu.kit.ipd.sdq.vitruvius.tests.mockup;
 
+import static org.junit.Assert.assertTrue;
+
 import java.lang.reflect.Field;
 import java.util.Map;
 import java.util.Set;
@@ -21,9 +23,9 @@ public class VSUMPersistentTest extends VSUMTest {
     public void testSaveAndLoadModels() throws Exception {
         // 1. create empty VSUM + store dummy model instances
         VSUMImpl vsum = testMetaRepositoryAndVSUMCreation();
-        int nrOfVURIs = 10;
+        int nrOfVURIs = 2;
         Set<VURI> vuris = PersistentTestUtil.createDummyVURIs(nrOfVURIs);
-        PersistentTestUtil.createResources(vuris);
+        // PersistentTestUtil.createResources(vuris);
         for (VURI vuri : vuris) {
             vsum.saveModelInstanceOriginal(vuri);
         }
@@ -31,6 +33,9 @@ public class VSUMPersistentTest extends VSUMTest {
         // 2.create VSUM again (should read all model instances from disk)
         VSUMImpl newVSUM = testMetaRepositoryAndVSUMCreation();
 
+        for (VURI vuri : vuris) {
+            assertTrue("new VSUM does not contain model instances with URI " + vuri, newVSUM.existsModelInstance(vuri));
+        }
         // 3. compare the modelInstances map from the two VSUMs - expectation: they are equal
         Map<VURI, ModelInstance> modelInstancesVSUM = getModelInstancesFieldFromVSUM(vsum);
         Map<VURI, ModelInstance> modelInstancesNewVSUM = getModelInstancesFieldFromVSUM(newVSUM);
