b68bce02cdbe6b11bb083806a2e88089db596ada
Felix Kutzner <felixkutzner@googlemail.com>
2014-07-15 11:57:11 +0000

    Deleted emfchange project, which is due to be replaced with a version from a private repository.

--- edu.kit.ipd.sdq.vitruvius.test.framework.run.editor.monitored.emfchange/src/edu/kit/ipd/sdq/vitruvius/framework/run/editor/monitored/emfchange/test/mocking/IEMFTreeEditor.java
+++
@@ -1,8 +0,0 @@
-package edu.kit.ipd.sdq.vitruvius.framework.run.editor.monitored.emfchange.test.mocking;
-
-import org.eclipse.emf.edit.domain.IEditingDomainProvider;
-import org.eclipse.ui.IEditorPart;
-
-public interface IEMFTreeEditor extends IEditorPart, IEditingDomainProvider {
-
-}
