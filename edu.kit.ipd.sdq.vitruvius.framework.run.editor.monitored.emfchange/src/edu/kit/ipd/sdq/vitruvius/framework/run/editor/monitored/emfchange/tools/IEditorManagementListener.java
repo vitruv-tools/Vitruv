608ea67e2000095072326dc97220608c8baedf91
Felix Kutzner <felixkutzner@googlemail.com>
2014-10-29 16:57:35 +0000

    Added the EPL license and EPL headers to *.java files.

--- edu.kit.ipd.sdq.vitruvius.framework.run.editor.monitored.emfchange/src/edu/kit/ipd/sdq/vitruvius/framework/run/editor/monitored/emfchange/tools/IEditorManagementListener.java
+++ edu.kit.ipd.sdq.vitruvius.framework.run.editor.monitored.emfchange/src/edu/kit/ipd/sdq/vitruvius/framework/run/editor/monitored/emfchange/tools/IEditorManagementListener.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Felix Kutzner.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Felix Kutzner - initial implementation.
+ ******************************************************************************/
+
 package edu.kit.ipd.sdq.vitruvius.framework.run.editor.monitored.emfchange.tools;
 
 import org.eclipse.ui.IEditorPart;
