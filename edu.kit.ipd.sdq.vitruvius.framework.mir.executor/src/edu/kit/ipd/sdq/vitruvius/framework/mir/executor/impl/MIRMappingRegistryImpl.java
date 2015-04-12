144650615e97d52165de1959ac7696d08d1c9862
Dominik Werle <dominik.werle@kit.edu>
2015-04-12 21:37:01 +0000

    Added Correspondence-to-MIRMapping-mapping to the interface/implementation.

--- edu.kit.ipd.sdq.vitruvius.framework.mir.executor/src/edu/kit/ipd/sdq/vitruvius/framework/mir/executor/impl/MIRMappingRegistryImpl.java
+++ edu.kit.ipd.sdq.vitruvius.framework.mir.executor/src/edu/kit/ipd/sdq/vitruvius/framework/mir/executor/impl/MIRMappingRegistryImpl.java
@@ -1,20 +1,25 @@
 package edu.kit.ipd.sdq.vitruvius.framework.mir.executor.impl;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes.CorrespondenceInstance;
 import edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes.EMFChangeResult;
 import edu.kit.ipd.sdq.vitruvius.framework.meta.change.EChange;
+import edu.kit.ipd.sdq.vitruvius.framework.meta.correspondence.Correspondence;
 import edu.kit.ipd.sdq.vitruvius.framework.mir.executor.interfaces.MIRMapping;
 import edu.kit.ipd.sdq.vitruvius.framework.mir.executor.interfaces.MIRMappingRegistry;
 import edu.kit.ipd.sdq.vitruvius.framework.mir.executor.interfaces.MIRModelInformationProvider;
 
 public class MIRMappingRegistryImpl implements MIRMappingRegistry {
 	private List<MIRMapping> mappings;
+	private Map<Correspondence, MIRMapping> correspondence2mapping;
 	
 	public MIRMappingRegistryImpl() {
 		mappings = new ArrayList<MIRMapping>();
+		correspondence2mapping = new HashMap<Correspondence, MIRMapping>();
 	}
 	
 	@Override
@@ -31,5 +36,16 @@ public class MIRMappingRegistryImpl implements MIRMappingRegistry {
 			result.addChangeResult(mapping.applyEChange(eChange, correspondenceInstance, modelInformationProvider));
 		}
 		return result;
+	}
+
+	@Override
+	public MIRMapping getMappingForCorrespondence(Correspondence correspondence) {
+		return correspondence2mapping.get(correspondence);
+	}
+
+	@Override
+	public void registerMappingForCorrespondence(MIRMapping mapping,
+			Correspondence correspondence) {
+		correspondence2mapping.put(correspondence, mapping);		
 	}
 }
--- edu.kit.ipd.sdq.vitruvius.framework.mir.executor/src/edu/kit/ipd/sdq/vitruvius/framework/mir/executor/interfaces/MIRMappingRegistry.java
+++ edu.kit.ipd.sdq.vitruvius.framework.mir.executor/src/edu/kit/ipd/sdq/vitruvius/framework/mir/executor/interfaces/MIRMappingRegistry.java
@@ -1,11 +1,9 @@
 package edu.kit.ipd.sdq.vitruvius.framework.mir.executor.interfaces;
 
-import org.eclipse.emf.ecore.EObject;
-
 import edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes.CorrespondenceInstance;
 import edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes.EMFChangeResult;
-import edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes.ModelInstance;
 import edu.kit.ipd.sdq.vitruvius.framework.meta.change.EChange;
+import edu.kit.ipd.sdq.vitruvius.framework.meta.correspondence.Correspondence;
 
 public interface MIRMappingRegistry {
 	public void addMapping(MIRMapping mapping);
@@ -13,4 +11,22 @@ public interface MIRMappingRegistry {
 	public EMFChangeResult applyAllMappings(EChange eChange,
 			CorrespondenceInstance correspondenceInstance,
 			MIRModelInformationProvider modelInformationProvider);
+	
+	/**
+	 * Returns the MIRMapping that created a correspondence, or null if
+	 * no mapping is coupled to the correspondence. To get all MIRMappings for an
+	 * EObject, first get all correspondences from the {@link CorrespondenceInstance},
+	 * then use this method.
+	 * @param correspondence
+	 * @return
+	 */
+	public MIRMapping getMappingForCorrespondence(Correspondence correspondence);
+	
+	/**
+	 * Register a mapping for a correspondence that can then be retrieved by calling
+	 * {@link #getMappingForCorrespondence(Correspondence)}.
+	 * @param mapping
+	 * @param correspondence
+	 */
+	public void registerMappingForCorrespondence(MIRMapping mapping, Correspondence correspondence);
 }
