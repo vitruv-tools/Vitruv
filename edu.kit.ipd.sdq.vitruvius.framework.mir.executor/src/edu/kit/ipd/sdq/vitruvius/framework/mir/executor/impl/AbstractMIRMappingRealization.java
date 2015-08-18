ee0c3562e5370044e4e77797282d21bbdbf8a2ac
Dominik Werle <dominik.werle@kit.edu>
2015-08-18 09:49:33 +0000

    Added handling of non contained EObjects. Clean up to support major change in structure.

--- edu.kit.ipd.sdq.vitruvius.framework.mir.executor/src/edu/kit/ipd/sdq/vitruvius/framework/mir/executor/impl/AbstractMIRMappingRealization.java
+++ edu.kit.ipd.sdq.vitruvius.framework.mir.executor/src/edu/kit/ipd/sdq/vitruvius/framework/mir/executor/impl/AbstractMIRMappingRealization.java
@@ -5,68 +5,90 @@ import static org.eclipse.xtext.xbase.lib.IterableExtensions.toList;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.apache.log4j.Logger;
 import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.transaction.RecordingCommand;
 import org.eclipse.xtext.xbase.lib.Functions;
 
 import edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes.Blackboard;
 import edu.kit.ipd.sdq.vitruvius.framework.meta.change.EChange;
+import edu.kit.ipd.sdq.vitruvius.framework.mir.executor.helpers.EclipseHelper;
 import edu.kit.ipd.sdq.vitruvius.framework.mir.executor.helpers.MIRMappingHelper;
 import edu.kit.ipd.sdq.vitruvius.framework.mir.executor.interfaces.MIRMappingRealization;
 import edu.kit.ipd.sdq.vitruvius.framework.mir.executor.interfaces.MappedCorrespondenceInstance;
+import edu.kit.ipd.sdq.vitruvius.framework.util.VitruviusConstants;
+import edu.kit.ipd.sdq.vitruvius.framework.util.bridges.EMFCommandBridge;
 import edu.kit.ipd.sdq.vitruvius.framework.util.datatypes.Pair;
 
 /**
- * {@link AbstractMIRMappingRealization} is extended by the code generated from the
- * intermediate language.
+ * {@link AbstractMIRMappingRealization} is extended by the code generated from
+ * the intermediate language.
  * <p>
- * A mapping is instantiated for two model instances. 
+ * A mapping is instantiated for two model instances.
+ * 
  * @author Dominik Werle
  *
  */
 public abstract class AbstractMIRMappingRealization implements MIRMappingRealization {
+	// TODO: get instance of command bridge
+	private EMFCommandBridge commandBridge = null;
+
 	private final static Logger LOGGER = Logger.getLogger(AbstractMIRMappingRealization.class);
-	
+
 	/**
 	 * Returns the {@link EClass} mapped by this mapping.
+	 * 
 	 * @return the {@link EClass} mapped by this mapping
 	 */
 	protected abstract EClass getMappedEClass();
-	
+
 	/**
-	 * Check if the conditions of the mapping hold for the given
-	 * {@link EObject}.
-	 * @param eObject the object to check
+	 * Check if the conditions of the mapping hold for the given {@link EObject}
+	 * .
+	 * 
+	 * @param eObject
+	 *            the object to check
 	 * @return true if the mapping holds for the given object.
 	 */
 	protected abstract boolean checkConditions(EObject eObject, Blackboard blackboard);
-	
+
 	/**
-	 * Ensure that the postconditions ("where") still hold for the
-	 * mapping.
+	 * Ensure that the postconditions ("where") still hold for the mapping.
 	 * <p>
-	 * The given {@link EChange} can be used to select the conditions
-	 * that have to be checked.
-	 * @param eObject the {@link EObject} that has been changed 
-	 * @param target the {@link EObject} that <code>eObject</code> is corresponding to
-	 * @param eChange the change that was applied
-	 * @return 
+	 * The given {@link EChange} can be used to select the conditions that have
+	 * to be checked.
+	 * 
+	 * @param eObject
+	 *            the {@link EObject} that has been changed
+	 * @param target
+	 *            the {@link EObject} that <code>eObject</code> is corresponding
+	 *            to
+	 * @param eChange
+	 *            the change that was applied
+	 * @return
 	 */
 	protected abstract List<Command> restorePostConditions(EObject eObject, EObject target, EChange change);
-	
+
 	/**
-	 * Creates a corresponding object for <code>eObject</code> and a correspondence in the mapped meta model
-	 * and registers it 
+	 * Creates a corresponding object for <code>eObject</code> and a
+	 * correspondence in the mapped meta model and registers it
 	 */
 	protected abstract List<Command> createCorresponding(EObject eObject, Blackboard blackboard);
-	
+
 	/**
-	 * Deletes the corresponding object (and its children) and the correspondence.
+	 * Deletes the corresponding object (and its children) and the
+	 * correspondence.
+	 * 
 	 * @param eObject
 	 * @param correspondenceInstance
 	 */
@@ -74,28 +96,25 @@ public abstract class AbstractMIRMappingRealization implements MIRMappingRealiza
 		// TODO: implement
 		throw new UnsupportedOperationException("Not implemented");
 	}
-	
+
 	/**
-	 * Returns {@link EObject EObjects} that are possibly affected by this change.
+	 * Returns {@link EObject EObjects} that are possibly affected by this
+	 * change.
+	 * 
 	 * @param eChange
 	 * @return
 	 */
 	protected Collection<EObject> getCandidates(EChange eChange) {
 		final EClass mappedEClass = getMappedEClass();
 		Collection<EObject> affectedObjects = MIRMappingHelper.getAllAffectedObjects(eChange);
-		return toList(filter(affectedObjects, new Functions.Function1<EObject, Boolean>() {
-			@Override
-			public Boolean apply(EObject p) {
-				return p.eClass().equals(mappedEClass);
-			}
-		}));
+		return toList(filter(affectedObjects, p -> p.eClass().equals(mappedEClass)));
 	}
-	
+
 	private MappedCorrespondenceInstance getMappedCorrespondenceInstanceFromBlackboard(Blackboard blackboard) {
 		// TODO: implement
 		throw new UnsupportedOperationException("Not implemented");
 	}
-	
+
 	@Override
 	public List<Command> applyEChange(EChange eChange, Blackboard blackboard) {
 		MappedCorrespondenceInstance correspondenceInstance = getMappedCorrespondenceInstanceFromBlackboard(blackboard);
@@ -114,6 +133,7 @@ public abstract class AbstractMIRMappingRealization implements MIRMappingRealiza
 			boolean mappedAfter = checkConditions(candidate, blackboard);
 
 			EObject mappingTarget = null;
+			final Collection<EObject> affectedEObjects = new HashSet<EObject>();
 
 			if (mappedBefore) {
 				mappingTarget = Objects.requireNonNull(correspondenceInstance.getMappingTarget(candidate, this));
@@ -124,15 +144,44 @@ public abstract class AbstractMIRMappingRealization implements MIRMappingRealiza
 			}
 
 			if (!mappedBefore && mappedAfter) {
-				result.addAll(createCorresponding(candidate, blackboard));
+				List<Command> createCorrespondingCommands = createCorresponding(candidate, blackboard);
+				affectedEObjects.addAll(
+						createCorrespondingCommands.stream()
+							.flatMap(it -> (Stream<EObject>)
+								(it.getAffectedObjects().stream()
+								   .filter(object -> object instanceof EObject)))
+							.collect(Collectors.toList()));
+				result.addAll(createCorrespondingCommands);
 				mappingTarget = Objects.requireNonNull(correspondenceInstance.getMappingTarget(candidate, this));
 			}
 
 			if (mappedAfter) {
 				result.addAll(restorePostConditions(candidate, mappingTarget, eChange));
 			}
+
+			result.addAll(handleNonContainedEObjects(affectedEObjects));
 		}
 
+		return result;
+	}
+
+	/**
+	 * Asks the user and creates new resources for EObjects in <code>affectedEObjects</code>
+	 * that do not have a container.
+	 */
+	private List<Command> handleNonContainedEObjects(Collection<EObject> affectedEObjects) {
+		if (affectedEObjects.isEmpty()) 
+			return Collections.emptyList();
+		
+		final List<Command> result = new ArrayList<Command>();
+		
+		for (final EObject eObject : affectedEObjects) {
+			if (eObject.eContainer() == null) {
+				Runnable saveRunnable = () -> EclipseHelper.askAndSaveResource(eObject);
+				result.add(commandBridge.createCommand(saveRunnable));
+			}
+		}
+		
 		return result;
 	}
 }
