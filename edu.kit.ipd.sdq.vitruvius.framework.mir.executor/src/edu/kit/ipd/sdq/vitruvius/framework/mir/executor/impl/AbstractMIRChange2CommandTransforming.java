ff7623c435056052c45557d5aa9c4b4c1a744fcf
Dominik Werle <dominik.werle@kit.edu>
2015-08-18 08:57:39 +0000

    Adapted MIR execution framework to major structural changes

---
+++ edu.kit.ipd.sdq.vitruvius.framework.mir.executor/src/edu/kit/ipd/sdq/vitruvius/framework/mir/executor/impl/AbstractMIRChange2CommandTransforming.java
@@ -0,0 +1,101 @@
+package edu.kit.ipd.sdq.vitruvius.framework.mir.executor.impl;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.eclipse.emf.common.command.Command;
+
+import edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes.Blackboard;
+import edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes.Change;
+import edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes.CompositeChange;
+import edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes.EMFModelChange;
+import edu.kit.ipd.sdq.vitruvius.framework.contracts.interfaces.Change2CommandTransforming;
+import edu.kit.ipd.sdq.vitruvius.framework.meta.change.EChange;
+import edu.kit.ipd.sdq.vitruvius.framework.mir.executor.interfaces.Invariant;
+import edu.kit.ipd.sdq.vitruvius.framework.mir.executor.interfaces.InvariantRegistry;
+import edu.kit.ipd.sdq.vitruvius.framework.mir.executor.interfaces.MIRMappingRealization;
+import edu.kit.ipd.sdq.vitruvius.framework.mir.executor.interfaces.Response;
+import edu.kit.ipd.sdq.vitruvius.framework.mir.executor.interfaces.ResponseRegistry;
+
+public abstract class AbstractMIRChange2CommandTransforming implements Change2CommandTransforming {
+    private final static Logger LOGGER = Logger.getLogger(AbstractMIRChange2CommandTransforming.class);
+
+    private final ResponseRegistry responseRegistry;
+    private final InvariantRegistry invariantRegistry;
+    private final Collection<MIRMappingRealization> mappings;
+
+    public AbstractMIRChange2CommandTransforming() {
+        this.responseRegistry = new ResponseRegistryImpl();
+        this.invariantRegistry = new InvariantRegistryImpl();
+        this.mappings = new HashSet<MIRMappingRealization>();
+
+        this.setup();
+    }
+
+    protected void addResponse(final Response response) {
+        this.responseRegistry.addResponse(response);
+    }
+
+    protected void addInvariant(final Invariant invariant) {
+        this.invariantRegistry.addInvariant(invariant);
+    }
+
+    protected void addMIRMapping(final MIRMappingRealization mapping) {
+        this.mappings.add(mapping);
+    }
+
+    @Override
+    public void transformChanges2Commands(Blackboard blackboard) {
+    	final List<Change> changes = blackboard.getAndArchiveChangesForTransformation();
+    	final List<Command> commands = new ArrayList<Command>();
+    	
+    	for (final Change change : changes) {
+    		commands.addAll(handleChange(change, blackboard)); 
+    	}
+    	
+    	blackboard.pushCommands(commands);
+    }
+
+    private List<Command> handleChange(Change change, Blackboard blackboard) {
+    	final List<Command> result = new ArrayList<Command>();
+        if (change instanceof CompositeChange) {
+            for (final Change c : ((CompositeChange) change).getChanges()) {
+            	result.addAll(handleChange(c, blackboard));
+            }
+        } else if (change instanceof EMFModelChange) {
+            result.addAll(handleEChange(((EMFModelChange) change).getEChange(), blackboard));
+        } else {
+            throw new IllegalArgumentException("Change subtype " + change.getClass().getName() + " not handled");
+        }
+        
+        return result;
+	}
+
+    protected List<Command> callRelevantMappings(final EChange eChange, Blackboard blackboard) {
+        final List<Command> result = new ArrayList<Command>();
+        final Collection<MIRMappingRealization> relevantMappings = this.getCandidateMappings(eChange);
+        for (final MIRMappingRealization mapping : relevantMappings) {
+            result.addAll(mapping.applyEChange(eChange, blackboard));
+        }
+        return result;
+    }
+
+    protected List<Command> handleEChange(final EChange eChange, Blackboard blackboard) {
+        return callRelevantMappings(eChange, blackboard);
+    }
+
+    /**
+     * Returns mappings that could be affected by the given {@link EChange}. Always returns a
+     * conservative estimate. Should be overwritten by the generated subclass of
+     * {@link AbstractMIRChange2CommandTransforming}, since the base implementation returns all
+     * mappings (i.e. the most conservative estimate).
+     */
+    protected Collection<MIRMappingRealization> getCandidateMappings(final EChange eChange) {
+        return this.mappings;
+    }
+
+    protected abstract void setup();
+}
--- edu.kit.ipd.sdq.vitruvius.framework.mir.executor/src/edu/kit/ipd/sdq/vitruvius/framework/mir/executor/impl/AbstractMIRMappingRealization.java
+++ edu.kit.ipd.sdq.vitruvius.framework.mir.executor/src/edu/kit/ipd/sdq/vitruvius/framework/mir/executor/impl/AbstractMIRMappingRealization.java
@@ -3,14 +3,18 @@ package edu.kit.ipd.sdq.vitruvius.framework.mir.executor.impl;
 import static org.eclipse.xtext.xbase.lib.IterableExtensions.filter;
 import static org.eclipse.xtext.xbase.lib.IterableExtensions.toList;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
 
 import org.apache.log4j.Logger;
+import org.eclipse.emf.common.command.Command;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtext.xbase.lib.Functions;
 
+import edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes.Blackboard;
 import edu.kit.ipd.sdq.vitruvius.framework.meta.change.EChange;
 import edu.kit.ipd.sdq.vitruvius.framework.mir.executor.helpers.MIRMappingHelper;
 import edu.kit.ipd.sdq.vitruvius.framework.mir.executor.interfaces.MIRMappingRealization;
@@ -38,10 +42,9 @@ public abstract class AbstractMIRMappingRealization implements MIRMappingRealiza
 	 * Check if the conditions of the mapping hold for the given
 	 * {@link EObject}.
 	 * @param eObject the object to check
-	 * @param correspondenceInstance
 	 * @return true if the mapping holds for the given object.
 	 */
-	protected abstract boolean checkConditions(EObject eObject, MappedCorrespondenceInstance correspondenceInstance);
+	protected abstract boolean checkConditions(EObject eObject, Blackboard blackboard);
 	
 	/**
 	 * Ensure that the postconditions ("where") still hold for the
@@ -54,25 +57,22 @@ public abstract class AbstractMIRMappingRealization implements MIRMappingRealiza
 	 * @param eChange the change that was applied
 	 * @return 
 	 */
-	protected abstract MIRMappingChangeResult restorePostConditions(EObject eObject, EObject target, EChange change);
+	protected abstract List<Command> restorePostConditions(EObject eObject, EObject target, EChange change);
 	
 	/**
 	 * Creates a corresponding object for <code>eObject</code> and a correspondence in the mapped meta model
 	 * and registers it 
-	 * @param eObject
-	 * @param correspondenceInstance
 	 */
-	protected abstract MIRMappingChangeResult createCorresponding(EObject eObject, MappedCorrespondenceInstance correspondenceInstance);
+	protected abstract List<Command> createCorresponding(EObject eObject, Blackboard blackboard);
 	
 	/**
 	 * Deletes the corresponding object (and its children) and the correspondence.
 	 * @param eObject
 	 * @param correspondenceInstance
 	 */
-	protected MIRMappingChangeResult deleteCorresponding(EObject eObject, EObject target, MappedCorrespondenceInstance correspondenceInstance) {
-		MIRMappingChangeResult result = new MIRMappingChangeResult();
-		result.addObjectToDelete(target);
-		return result;
+	protected List<Command> deleteCorresponding(EObject eObject, EObject target, Blackboard blackboard) {
+		// TODO: implement
+		throw new UnsupportedOperationException("Not implemented");
 	}
 	
 	/**
@@ -84,57 +84,55 @@ public abstract class AbstractMIRMappingRealization implements MIRMappingRealiza
 		final EClass mappedEClass = getMappedEClass();
 		Collection<EObject> affectedObjects = MIRMappingHelper.getAllAffectedObjects(eChange);
 		return toList(filter(affectedObjects, new Functions.Function1<EObject, Boolean>() {
-			@Override public Boolean apply(EObject p) {
+			@Override
+			public Boolean apply(EObject p) {
 				return p.eClass().equals(mappedEClass);
 			}
 		}));
 	}
 	
-	private EObject getNewCorresponding(EObject source, MIRMappingChangeResult changeResult) {
-		for (Pair<EObject, EObject> candidate : changeResult.getCorrespondencesToAdd()) {
-			if (candidate.getFirst() == source)
-				return candidate.getSecond();
-			else if (candidate.getSecond() == source)
-				return candidate.getFirst();
-		}
-			
-		return null;
+	private MappedCorrespondenceInstance getMappedCorrespondenceInstanceFromBlackboard(Blackboard blackboard) {
+		// TODO: implement
+		throw new UnsupportedOperationException("Not implemented");
 	}
 	
 	@Override
-	public MIRMappingChangeResult applyEChange(
-			EChange eChange,
-			MappedCorrespondenceInstance correspondenceInstance) {
+	public List<Command> applyEChange(EChange eChange, Blackboard blackboard) {
+		MappedCorrespondenceInstance correspondenceInstance = getMappedCorrespondenceInstanceFromBlackboard(blackboard);
+
+		/*
+		 * TODO: change to create candidates (EObject, PotentialTransition(new,
+		 * still, remove), Mapping) in AbstractMIRChange2CommandTransforming.
+		 */
 		Collection<EObject> candidates = getCandidates(eChange);
-		
-		MIRMappingChangeResult result = new MIRMappingChangeResult();
-		
+		List<Command> result = new ArrayList<Command>();
+
 		for (EObject candidate : candidates) {
 			LOGGER.trace("Checking candidate " + candidate.toString());
-			
+
 			boolean mappedBefore = correspondenceInstance.checkIfMappedBy(candidate, this);
-			boolean mappedAfter = checkConditions(candidate, correspondenceInstance);
-			
+			boolean mappedAfter = checkConditions(candidate, blackboard);
+
 			EObject mappingTarget = null;
-			
+
 			if (mappedBefore) {
 				mappingTarget = Objects.requireNonNull(correspondenceInstance.getMappingTarget(candidate, this));
-				
+
 				if (!mappedAfter) {
-					result.add(deleteCorresponding(candidate, mappingTarget, correspondenceInstance));
+					result.addAll(deleteCorresponding(candidate, mappingTarget, blackboard));
 				}
 			}
-			
+
 			if (!mappedBefore && mappedAfter) {
-				result.add(createCorresponding(candidate, correspondenceInstance));
-				mappingTarget = Objects.requireNonNull(getNewCorresponding(candidate, result));
+				result.addAll(createCorresponding(candidate, blackboard));
+				mappingTarget = Objects.requireNonNull(correspondenceInstance.getMappingTarget(candidate, this));
 			}
-			
+
 			if (mappedAfter) {
-				result.add(restorePostConditions(candidate, mappingTarget, eChange));
+				result.addAll(restorePostConditions(candidate, mappingTarget, eChange));
 			}
 		}
-		
+
 		return result;
 	}
 }
