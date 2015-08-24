2654481a445a52c24c1598d0a441ddf9c97c1fda
Max Kramer <max.kramer@kit.edu>
2015-08-24 07:30:48 +0000

    documentation and minor syntax change

--- edu.kit.ipd.sdq.vitruvius.framework.contracts/src/edu/kit/ipd/sdq/vitruvius/framework/contracts/datatypes/Blackboard.java
+++ edu.kit.ipd.sdq.vitruvius.framework.contracts/src/edu/kit/ipd/sdq/vitruvius/framework/contracts/datatypes/Blackboard.java
@@ -7,19 +7,6 @@ import org.eclipse.emf.common.command.Command;
 import edu.kit.ipd.sdq.vitruvius.framework.contracts.interfaces.ModelProviding;
 import edu.kit.ipd.sdq.vitruvius.framework.util.datatypes.Pair;
 
-/**
- * Blackboard managing all data that is necessary to propagate a list of changes that occurred in
- * model instances of a metamodel A to model instances of a metamodel B. A new individual blackboard
- * is used to propagate other changes to model instances of B or the same changes to model instances
- * of another metamodel C. All blackboards are kept in an archive for later undoing and redoing.
- * <p>
- * The method invocation protocol for each blackboard is depicted in the following state chart:
- * <p>
- * <img src="doc-files/Blackboard.png">
- * 
- * @author kramerm
- *
- */
 /*
  * @startuml doc-files/Blackboard.png
  *
@@ -52,6 +39,19 @@ import edu.kit.ipd.sdq.vitruvius.framework.util.datatypes.Pair;
  * Ready --> Ready : getCorrespondenceInstance, getModelProviding
  *
  * @enduml
+ */
+/**
+ * Blackboard managing all data that is necessary to propagate a list of changes that occurred in
+ * model instances of a metamodel A to model instances of a metamodel B. A new individual blackboard
+ * is used to propagate other changes to model instances of B or the same changes to model instances
+ * of another metamodel C. All blackboards are kept in an archive for later undoing and redoing.
+ * <p>
+ * The method invocation protocol for each blackboard is depicted in the following state chart:
+ * <p>
+ * <img src="doc-files/Blackboard.png">
+ *
+ * @author kramerm
+ *
  */
 public interface Blackboard {
     CorrespondenceInstance getCorrespondenceInstance();
