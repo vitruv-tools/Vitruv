74f64c8da881a290a2fc6e5ac02367a0c9f234dd
Michael Langhammer <langhammer@kit.edu>
2015-08-25 08:56:57 +0000

    adapted to new ModelProviding

--- edu.kit.ipd.sdq.vitruvius.tests.casestudies.jmljava/src-test/edu/kit/ipd/sdq/vitruvius/tests/casestudies/jmljava/unittests/utils/ModelProvidingMock.java
+++ edu.kit.ipd.sdq.vitruvius.tests.casestudies.jmljava/src-test/edu/kit/ipd/sdq/vitruvius/tests/casestudies/jmljava/unittests/utils/ModelProvidingMock.java
@@ -3,29 +3,36 @@ package edu.kit.ipd.sdq.vitruvius.tests.casestudies.jmljava.unittests.utils;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
+
 import edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes.ModelInstance;
 import edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes.VURI;
 import edu.kit.ipd.sdq.vitruvius.framework.contracts.interfaces.ModelProviding;
 
 public class ModelProvidingMock implements ModelProviding {
     final Map<VURI, ModelInstance> mapping = new HashMap<VURI, ModelInstance>();
-            
-    public void add(ModelInstance model) {
-        mapping.put(model.getURI(), model);
-    }
-    
-    @Override
-    public ModelInstance getAndLoadModelInstanceOriginal(VURI uri) {
-        return mapping.get(uri);
+
+    public void add(final ModelInstance model) {
+        this.mapping.put(model.getURI(), model);
     }
 
     @Override
-    public ModelInstance getModelInstanceCopy(VURI uri) {
+    public ModelInstance getAndLoadModelInstanceOriginal(final VURI uri) {
+        return this.mapping.get(uri);
+    }
+
+    @Override
+    public ModelInstance getModelInstanceCopy(final VURI uri) {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public void saveModelInstanceOriginal(VURI vuri) {
+    public void saveModelInstanceOriginal(final VURI vuri) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public TransactionalEditingDomain getEditingDomain() {
         throw new UnsupportedOperationException();
     }
 }
