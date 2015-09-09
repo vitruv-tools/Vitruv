0bed15ddc5a9c64a13ab0ed8d8ebed2df76ee1ab
Michael Langhammer <langhammer@kit.edu>
2015-09-09 08:38:36 +0000

    remove basic integration.test project because all tests are in pcm java case study

--- edu.kit.ipd.sdq.vitruvius.integration.tests.casestudies/src/edu/kit/ipd/sdq/vitruvius/integration/tests/casestudies/correspondence/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * This package contains JUnit tests for the correspondence model that is built during the integration of java code.
- *
- */
-package edu.kit.ipd.sdq.vitruvius.integration.tests.casestudies.correspondence;
