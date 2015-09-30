77de7af7ae9c3b99795656661d1c395747e4b009
Dominik Werle <dominik.werle@kit.edu>
2015-09-30 12:03:38 +0000

    Removed erroneous getAffectedObjects

--- edu.kit.ipd.sdq.vitruvius.framework.contracts/src/edu/kit/ipd/sdq/vitruvius/framework/contracts/util/datatypes/VitruviusRecordingCommand.java
+++ edu.kit.ipd.sdq.vitruvius.framework.contracts/src/edu/kit/ipd/sdq/vitruvius/framework/contracts/util/datatypes/VitruviusRecordingCommand.java
@@ -1,19 +1,12 @@
 package edu.kit.ipd.sdq.vitruvius.framework.contracts.util.datatypes;
 
 import java.lang.reflect.Modifier;
-import java.util.Collection;
-import java.util.Collections;
 
 import org.apache.log4j.Logger;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.transaction.RecordingCommand;
-import org.eclipse.emf.transaction.Transaction;
-import org.eclipse.emf.transaction.TransactionChangeDescription;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
 
 import edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes.TransformationResult;
-import edu.kit.ipd.sdq.vitruvius.framework.contracts.util.bridges.EMFChangeBridge;
-import edu.kit.ipd.sdq.vitruvius.framework.util.bridges.JavaBridge;
 
 public abstract class VitruviusRecordingCommand extends RecordingCommand {
 
@@ -48,20 +41,6 @@ public abstract class VitruviusRecordingCommand extends RecordingCommand {
 
     public TransformationResult getTransformationResult() {
         return this.transformationResult;
-    }
-
-    @Override
-    public Collection<?> getAffectedObjects() {
-        Transaction transaction = JavaBridge.getFieldFromClass(RecordingCommand.class, "transaction", this);
-        if (transaction == null) {
-            // TODO DW what to do, if transaction is null? when is this the case?
-            return Collections.EMPTY_SET;
-        }
-
-        final TransactionChangeDescription changeDescription = transaction.getChangeDescription();
-        final Collection<EObject> affectedEObjects = EMFChangeBridge.getAffectedObjects(changeDescription);
-
-        return affectedEObjects;
     }
 
     @Override
