b0393cff1454916994c786f79f87fea3db47fc89
Frederik Petersen <frederik.petersen@student.kit.edu>
2016-02-13 10:52:09 +0000

    Added some more util functions to code integration test

--- edu.kit.ipd.sdq.vitruvius.codeintegration.tests/src/edu/kit/ipd/sdq/vitruvius/codeintegration/tests/CodeIntegrationTest.java
+++ edu.kit.ipd.sdq.vitruvius.codeintegration.tests/src/edu/kit/ipd/sdq/vitruvius/codeintegration/tests/CodeIntegrationTest.java
@@ -6,20 +6,34 @@ import java.lang.reflect.InvocationTargetException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.*;
 
+import org.apache.log4j.Logger;
+import org.eclipse.core.internal.events.BuildCommand;
+import org.eclipse.core.internal.resources.Project;
+import org.eclipse.core.internal.resources.ProjectDescription;
+import org.eclipse.core.internal.resources.ProjectInfo;
+import org.eclipse.core.internal.resources.ResourceInfo;
+import org.eclipse.core.resources.ICommand;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.ui.dialogs.IOverwriteQuery;
 import org.eclipse.ui.wizards.datatransfer.FileSystemStructureProvider;
 import org.eclipse.ui.wizards.datatransfer.ImportOperation;
@@ -29,9 +43,21 @@ import org.junit.Before;
 import org.junit.Test;
 import org.osgi.framework.Bundle;
 
+import edu.kit.ipd.sdq.vitruvius.casestudies.emf.builder.VitruviusEmfBuilder;
+import edu.kit.ipd.sdq.vitruvius.casestudies.pcmjava.PCMJaMoPPNamespace;
+import edu.kit.ipd.sdq.vitruvius.casestudies.pcmjava.builder.PCMJavaAddBuilder;
+import edu.kit.ipd.sdq.vitruvius.casestudies.pcmjava.builder.PCMJavaBuilder;
+import edu.kit.ipd.sdq.vitruvius.codeintegration.ResourceLoadingHelper;
 import edu.kit.ipd.sdq.vitruvius.codeintegration.ui.commands.IntegrateProjectHandler;
+import edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes.CorrespondenceInstance;
+import edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes.VURI;
+import edu.kit.ipd.sdq.vitruvius.framework.contracts.meta.correspondence.Correspondence;
+import edu.kit.ipd.sdq.vitruvius.framework.util.bridges.JavaBridge;
+import edu.kit.ipd.sdq.vitruvius.framework.vsum.VSUMImpl;
 
 public class CodeIntegrationTest {
+	
+    private static final Logger logger = Logger.getLogger(CodeIntegrationTest.class.getSimpleName());
 	
 	private static final String TEST_BUNDLE_NAME = "edu.kit.ipd.sdq.vitruvius.codeintegration.tests";
 	private static final String TEST_PROJECT_NAME = "eu.fpetersen.cbs.pc";
@@ -100,7 +126,7 @@ public class CodeIntegrationTest {
 	}
 	
 	@Test
-	public void testStandardCodeIntegration() throws CoreException, InterruptedException {
+	public void testStandardCodeIntegration() throws Throwable {
 		IntegrateProjectHandler integrateProjectHander = new IntegrateProjectHandler();
 		integrateProjectHander.integrateProject(testProject);
 		
@@ -123,7 +149,58 @@ public class CodeIntegrationTest {
 		IFolder vsumFolder = metaProject.getFolder("vsum");
 		Assert.assertNotNull(vsumFolder);
 		
+		// add PCM Java Builder to Project under test
+        final PCMJavaAddBuilder pcmJavaBuilder = new PCMJavaAddBuilder();
+        pcmJavaBuilder.addBuilderToProject(testProject);
+        // build the project
+        progress = new DoneFlagProgressMonitor();
+        testProject.build(IncrementalProjectBuilder.INCREMENTAL_BUILD, PCMJaMoPPNamespace.BUILDER_ID, new HashMap<String, String>(),
+                progress);
+        while (!progress.isDone()) {
+			Thread.sleep(100);
+		}
+        
+        CorrespondenceInstance ci = getCorrespondenceInstance();
+        //TODO check if correspondences are correct
+		
 	}
+	
+    private CorrespondenceInstance getCorrespondenceInstance() throws Throwable {
+        final VSUMImpl vsum = this.getVSUM();
+        final VURI jaMoPPVURI = VURI.getInstance(PCMJaMoPPNamespace.JaMoPP.JAMOPP_METAMODEL_NAMESPACE);
+        final VURI pcmVURI = VURI.getInstance(PCMJaMoPPNamespace.PCM.PCM_METAMODEL_NAMESPACE);
+        final CorrespondenceInstance corresponcenceInstance = vsum.getCorrespondenceInstanceOriginal(pcmVURI,
+                jaMoPPVURI);
+        return corresponcenceInstance;
+    }
+
+    private VSUMImpl getVSUM() throws Throwable {
+        final PCMJavaBuilder pcmJavaBuilder = this.getPCMJavaBuilderFromProject();
+        if (null == pcmJavaBuilder) {
+            return null;
+        }
+        final VSUMImpl vsum = JavaBridge.getFieldFromClass(VitruviusEmfBuilder.class, "vsum", pcmJavaBuilder);
+        return vsum;
+    }
+
+    @SuppressWarnings("restriction")
+	private PCMJavaBuilder getPCMJavaBuilderFromProject() throws Throwable {
+        final Project project = (Project) testProject;
+        final ResourceInfo info = project.getResourceInfo(false, false);
+        final ProjectDescription description = ((ProjectInfo) info).getDescription();
+        final boolean makeCopy = false;
+        for (final ICommand command : description.getBuildSpec(makeCopy)) {
+            if (command.getBuilderName().equals(PCMJaMoPPNamespace.BUILDER_ID)) {
+                final BuildCommand buildCommand = (BuildCommand) command;
+                final IncrementalProjectBuilder ipb = buildCommand
+                        .getBuilder(testProject.getActiveBuildConfig());
+                final PCMJavaBuilder pcmJavaBuilder = (PCMJavaBuilder) ipb;
+                return pcmJavaBuilder;
+            }
+        }
+        logger.warn("Could not find any PCMJavaBuilder");
+        return null;
+    }
 	
 	/**
 	 * Thread-safe simple progress monitor for knowing when a job is done.
