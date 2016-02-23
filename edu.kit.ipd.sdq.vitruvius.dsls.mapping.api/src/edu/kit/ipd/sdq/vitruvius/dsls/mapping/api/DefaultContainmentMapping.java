c41cd919c9fe6126c0a38c65dacf31005878cae7
Dominik Werle <dominik.werle@kit.edu>
2016-02-23 13:30:13 +0000

    Changed MIRUserInteracting to UserInteracting

--- edu.kit.ipd.sdq.vitruvius.dsls.mapping.api/src/edu/kit/ipd/sdq/vitruvius/dsls/mapping/api/DefaultContainmentMapping.java
+++ edu.kit.ipd.sdq.vitruvius.dsls.mapping.api/src/edu/kit/ipd/sdq/vitruvius/dsls/mapping/api/DefaultContainmentMapping.java
@@ -1,26 +1,20 @@
 package edu.kit.ipd.sdq.vitruvius.dsls.mapping.api;
 
-import edu.kit.ipd.sdq.vitruvius.dsls.mapping.api.interfaces.MIRUserInteracting;
 import edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes.Blackboard;
 import edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes.VURI;
+import edu.kit.ipd.sdq.vitruvius.framework.contracts.interfaces.UserInteracting;
 import edu.kit.ipd.sdq.vitruvius.framework.meta.change.EChange;
 import edu.kit.ipd.sdq.vitruvius.framework.util.bridges.EcoreBridge;
 
 public class DefaultContainmentMapping extends AbstractMappingRealization {
 	public final static DefaultContainmentMapping INSTANCE = new DefaultContainmentMapping();
 	
-	private MIRUserInteracting userInteracting = new EclipseDialogMIRUserInteracting();
-
-	public void setUserInteracting(MIRUserInteracting userInteracting) {
-		this.userInteracting = userInteracting;
-	}
-	
 	@Override
 	public void applyEChange(EChange eChange, Blackboard blackboard, MappingExecutionState state) {
 		MIRMappingHelper.ensureContainments(state.getTransformationResult(), state::getAllAffectedEObjects, (objectToCreateContainmentFor) -> {
 			state.addObjectForTuidUpdate(objectToCreateContainmentFor);
 			state.getTransformationResult().addRootEObjectToSave(objectToCreateContainmentFor, VURI.getInstance(
-					userInteracting.askForNewResource(EcoreBridge.createSensibleString(objectToCreateContainmentFor))));
+					state.getUserInteracting().selectURI(EcoreBridge.createSensibleString(objectToCreateContainmentFor))));
 		});
 		
 		state.updateAllTuidsOfCachedObjects();
