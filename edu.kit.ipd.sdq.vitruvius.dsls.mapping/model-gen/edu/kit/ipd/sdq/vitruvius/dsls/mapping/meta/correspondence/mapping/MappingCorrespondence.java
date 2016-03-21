e8d10753aafa1ba7865d61e054f7c3b03c88ce8a
Heiko Klare <heiko.klare@kit.edu>
2016-03-21 08:19:50 +0000

    Generate MappedCorrespondence to model-gen folder to avoid getting overridden by Xtext workflow. Regenerate Mapping language due to changed MirBaseLanguage.

---
+++ edu.kit.ipd.sdq.vitruvius.dsls.mapping/model-gen/edu/kit/ipd/sdq/vitruvius/dsls/mapping/meta/correspondence/mapping/MappingCorrespondence.java
@@ -0,0 +1,18 @@
+/**
+ */
+package edu.kit.ipd.sdq.vitruvius.dsls.mapping.meta.correspondence.mapping;
+
+import edu.kit.ipd.sdq.vitruvius.framework.contracts.meta.correspondence.Correspondence;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Correspondence</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see edu.kit.ipd.sdq.vitruvius.dsls.mapping.meta.correspondence.mapping.MappingPackage#getMappingCorrespondence()
+ * @model
+ * @generated
+ */
+public interface MappingCorrespondence extends Correspondence {
+} // MappingCorrespondence
---
+++ edu.kit.ipd.sdq.vitruvius.dsls.mapping/model-gen/edu/kit/ipd/sdq/vitruvius/dsls/mapping/meta/correspondence/mapping/impl/MappingCorrespondenceImpl.java
@@ -0,0 +1,39 @@
+/**
+ */
+package edu.kit.ipd.sdq.vitruvius.dsls.mapping.meta.correspondence.mapping.impl;
+
+import edu.kit.ipd.sdq.vitruvius.dsls.mapping.meta.correspondence.mapping.MappingCorrespondence;
+import edu.kit.ipd.sdq.vitruvius.dsls.mapping.meta.correspondence.mapping.MappingPackage;
+
+import edu.kit.ipd.sdq.vitruvius.framework.contracts.meta.correspondence.impl.CorrespondenceImpl;
+
+import org.eclipse.emf.ecore.EClass;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Correspondence</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * @generated
+ */
+public class MappingCorrespondenceImpl extends CorrespondenceImpl implements MappingCorrespondence {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected MappingCorrespondenceImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return MappingPackage.Literals.MAPPING_CORRESPONDENCE;
+	}
+
+} //MappingCorrespondenceImpl
