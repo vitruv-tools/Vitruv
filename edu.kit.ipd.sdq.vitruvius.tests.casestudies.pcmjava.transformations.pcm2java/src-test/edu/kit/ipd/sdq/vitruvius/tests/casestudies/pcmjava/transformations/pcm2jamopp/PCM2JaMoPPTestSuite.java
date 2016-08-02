d33c1316c8254fc2c77ac36a7383853a50c09de0
Heiko Klare <heiko.klare@kit.edu>
2016-08-02 07:31:25 +0000

    Add PCM to Java tests implementation to new project.

---
+++ edu.kit.ipd.sdq.vitruvius.tests.casestudies.pcmjava.transformations.pcm2java/src-test/edu/kit/ipd/sdq/vitruvius/tests/casestudies/pcmjava/transformations/pcm2jamopp/PCM2JaMoPPTestSuite.java
@@ -0,0 +1,32 @@
+package edu.kit.ipd.sdq.vitruvius.tests.casestudies.pcmjava.transformations.pcm2jamopp;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import edu.kit.ipd.sdq.vitruvius.tests.casestudies.pcmjava.transformations.pcm2jamopp.repository.BasicComponentMappingTransformationTest;
+import edu.kit.ipd.sdq.vitruvius.tests.casestudies.pcmjava.transformations.pcm2jamopp.repository.CollectionDataTypeMappingTransformationTest;
+import edu.kit.ipd.sdq.vitruvius.tests.casestudies.pcmjava.transformations.pcm2jamopp.repository.CompositeComponentMappingTransformationTest;
+import edu.kit.ipd.sdq.vitruvius.tests.casestudies.pcmjava.transformations.pcm2jamopp.repository.CompositeDataTypeMappingTransformationTest;
+import edu.kit.ipd.sdq.vitruvius.tests.casestudies.pcmjava.transformations.pcm2jamopp.repository.InnerDeclarationMappingTransformationTest;
+import edu.kit.ipd.sdq.vitruvius.tests.casestudies.pcmjava.transformations.pcm2jamopp.repository.OperationInterfaceMappingTransformationTest;
+import edu.kit.ipd.sdq.vitruvius.tests.casestudies.pcmjava.transformations.pcm2jamopp.repository.OperationProvidedRoleMappingTransformationTest;
+import edu.kit.ipd.sdq.vitruvius.tests.casestudies.pcmjava.transformations.pcm2jamopp.repository.OperationRequiredRoleMappingTransformationTest;
+import edu.kit.ipd.sdq.vitruvius.tests.casestudies.pcmjava.transformations.pcm2jamopp.repository.OperationSignatureMappingTransformationTest;
+import edu.kit.ipd.sdq.vitruvius.tests.casestudies.pcmjava.transformations.pcm2jamopp.repository.PCMParameterMappingTransformationTest;
+import edu.kit.ipd.sdq.vitruvius.tests.casestudies.pcmjava.transformations.pcm2jamopp.repository.RepositoryMappingTransformaitonTest;
+import edu.kit.ipd.sdq.vitruvius.tests.casestudies.pcmjava.transformations.pcm2jamopp.repository.ResourceDemandingInternalBehaviorMappingTransformationTest;
+import edu.kit.ipd.sdq.vitruvius.tests.casestudies.pcmjava.transformations.pcm2jamopp.repository.SEFFMappingTransformationTest;
+
+@RunWith(Suite.class)
+
+@SuiteClasses({ BasicComponentMappingTransformationTest.class, CollectionDataTypeMappingTransformationTest.class,
+		CompositeComponentMappingTransformationTest.class, CompositeDataTypeMappingTransformationTest.class,
+		InnerDeclarationMappingTransformationTest.class, OperationInterfaceMappingTransformationTest.class,
+		OperationProvidedRoleMappingTransformationTest.class, OperationRequiredRoleMappingTransformationTest.class,
+		OperationSignatureMappingTransformationTest.class, PCMParameterMappingTransformationTest.class,
+		RepositoryMappingTransformaitonTest.class, ResourceDemandingInternalBehaviorMappingTransformationTest.class,
+		SEFFMappingTransformationTest.class })
+public class PCM2JaMoPPTestSuite {
+
+}
---
+++ edu.kit.ipd.sdq.vitruvius.tests.casestudies.pcmjava.transformations.pcm2java/src-test/edu/kit/ipd/sdq/vitruvius/tests/casestudies/pcmjava/transformations/pcm2jamopp/system/SystemMappingTransformationTest.java
@@ -0,0 +1,55 @@
+package edu.kit.ipd.sdq.vitruvius.tests.casestudies.pcmjava.transformations.pcm2jamopp.system;
+
+import static org.junit.Assert.fail;
+
+import org.emftext.language.java.containers.CompilationUnit;
+import org.emftext.language.java.containers.Package;
+import org.junit.Test;
+import org.palladiosimulator.pcm.system.System;
+
+import edu.kit.ipd.sdq.vitruvius.framework.util.bridges.EcoreResourceBridge;
+import edu.kit.ipd.sdq.vitruvius.tests.casestudies.pcmjava.transformations.pcm2jamopp.PCM2JaMoPPTransformationTest;
+import edu.kit.ipd.sdq.vitruvius.tests.casestudies.pcmjava.transformations.utils.PCM2JaMoPPTestUtils;
+
+public class SystemMappingTransformationTest extends PCM2JaMoPPTransformationTest {
+
+    @Test
+    public void testCreateSystem() throws Throwable {
+        final System system = super.createAndSyncSystem(PCM2JaMoPPTestUtils.SYSTEM_NAME);
+
+        this.assertSystem(system);
+    }
+
+    @Test
+    public void testRenameSystem() throws Throwable {
+        final System system = super.createAndSyncSystem(PCM2JaMoPPTestUtils.SYSTEM_NAME);
+
+        system.setEntityName(PCM2JaMoPPTestUtils.SYSTEM_NAME + PCM2JaMoPPTestUtils.RENAME);
+        EcoreResourceBridge.saveResource(system.eResource());
+        super.triggerSynchronization(system);
+
+        this.assertSystem(system);
+    }
+
+    @Test
+    public void testRemoveSystem() throws Throwable {
+        final System system = PCM2JaMoPPTestUtils.createSystem(this.resourceSet, PCM2JaMoPPTestUtils.SYSTEM_NAME,
+                this.currentTestProjectName);
+
+        fail("Not yet implemented");
+    }
+
+    /**
+     * a system should correspond to a Package with its name and a class with its name "Impl"
+     *
+     * @param system
+     * @throws Throwable
+     */
+    @SuppressWarnings("unchecked")
+    private void assertSystem(final System system) throws Throwable {
+        final String expectedName = system.getEntityName();
+        this.assertCorrespondnecesAndCompareNames(system, 3,
+                new Class[] { Package.class, CompilationUnit.class, org.emftext.language.java.classifiers.Class.class },
+                new String[] { expectedName, expectedName + "Impl", expectedName + "Impl" });
+    }
+}
