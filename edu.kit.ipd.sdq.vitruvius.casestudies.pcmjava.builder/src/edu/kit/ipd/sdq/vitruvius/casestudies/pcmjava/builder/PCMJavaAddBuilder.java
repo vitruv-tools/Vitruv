8570489e1a118007f0ea872fbd6c7ea910d87400
Heiko Klare <heiko.klare@kit.edu>
2016-08-02 07:25:52 +0000

    Move PCM-Java builder classes to new project.

---
+++ edu.kit.ipd.sdq.vitruvius.casestudies.pcmjava.builder/src/edu/kit/ipd/sdq/vitruvius/casestudies/pcmjava/builder/PCMJavaAddBuilder.java
@@ -0,0 +1,12 @@
+package edu.kit.ipd.sdq.vitruvius.casestudies.pcmjava.builder;
+
+import edu.kit.ipd.sdq.vitruvius.casestudies.emf.builder.AddBuilder;
+import edu.kit.ipd.sdq.vitruvius.casestudies.pcmjava.PCMJaMoPPNamespace;
+
+public class PCMJavaAddBuilder extends AddBuilder {
+
+    public PCMJavaAddBuilder() {
+        super(PCMJaMoPPNamespace.BUILDER_ID);
+    }
+
+}
---
+++ edu.kit.ipd.sdq.vitruvius.casestudies.pcmjava.builder/src/edu/kit/ipd/sdq/vitruvius/casestudies/pcmjava/builder/PCMJavaRemoveBuilder.java
@@ -0,0 +1,11 @@
+package edu.kit.ipd.sdq.vitruvius.casestudies.pcmjava.builder;
+
+import edu.kit.ipd.sdq.vitruvius.casestudies.emf.builder.RemoveBuilder;
+import edu.kit.ipd.sdq.vitruvius.casestudies.pcmjava.PCMJaMoPPNamespace;
+
+public class PCMJavaRemoveBuilder extends RemoveBuilder {
+
+    public PCMJavaRemoveBuilder() {
+        super(PCMJaMoPPNamespace.BUILDER_ID);
+    }
+}
