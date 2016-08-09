18657c88bf518c2cdb3b70000f61dbc4aed078ec
Heiko Klare <heiko.klare@kit.edu>
2016-08-09 13:52:09 +0000

    Adapt ChangeSubmitter to new VitruviusChange hierarchy.

--- edu.kit.ipd.sdq.vitruvius.casestudies.java.run.monitorededitor/src/edu/kit/ipd/sdq/vitruvius/framework/model/monitor/ChangeSubmitter.java
+++ edu.kit.ipd.sdq.vitruvius.casestudies.java.run.monitorededitor/src/edu/kit/ipd/sdq/vitruvius/framework/model/monitor/ChangeSubmitter.java
@@ -1,9 +1,9 @@
 package edu.kit.ipd.sdq.vitruvius.framework.model.monitor;
 
-import edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes.Change;
+import edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes.VitruviusChange;
 
 public interface ChangeSubmitter {
 
-    void submitChange(final Change change);
+    void submitChange(final VitruviusChange change);
 
 }
