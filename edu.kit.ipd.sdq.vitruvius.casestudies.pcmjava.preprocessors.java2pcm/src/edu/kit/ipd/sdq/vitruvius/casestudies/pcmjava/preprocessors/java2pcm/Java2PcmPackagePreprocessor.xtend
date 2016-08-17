9202bf3d117ec5ae679994abb4b7ce6246b06e08
Heiko Klare <heiko.klare@kit.edu>
2016-08-17 15:42:13 +0000

    Add Java to PCM preprocessors for method body changes and package preprocessing.

---
+++ edu.kit.ipd.sdq.vitruvius.casestudies.pcmjava.preprocessors.java2pcm/src/edu/kit/ipd/sdq/vitruvius/casestudies/pcmjava/preprocessors/java2pcm/Java2PcmPackagePreprocessor.xtend
@@ -0,0 +1,74 @@
+package edu.kit.ipd.sdq.vitruvius.casestudies.pcmjava.preprocessors.java2pcm
+
+import edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes.VURI
+import org.eclipse.emf.ecore.EObject
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl
+import org.eclipse.emf.ecore.resource.ResourceSet
+import org.emftext.language.java.containers.Package
+import org.eclipse.emf.ecore.resource.Resource
+import edu.kit.ipd.sdq.vitruvius.dsls.response.runtime.Change2CommandTransformingPreprocessor
+import edu.kit.ipd.sdq.vitruvius.framework.contracts.interfaces.UserInteracting
+import edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes.Blackboard
+import edu.kit.ipd.sdq.vitruvius.framework.contracts.change.GeneralChange
+import edu.kit.ipd.sdq.vitruvius.framework.contracts.meta.change.root.InsertRootEObject
+import edu.kit.ipd.sdq.vitruvius.framework.contracts.meta.change.javaextension.change.feature.attribute.JavaReplaceSingleValuedEAttribute
+import edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes.VitruviusChange
+
+class Java2PcmPackagePreprocessor implements Change2CommandTransformingPreprocessor {
+	    /**
+     * Special treatment for packages: we have to use the package-info file as input for the
+     * transformation and make sure that the packages have resources attached
+     *
+     * @param change
+     *            the change that may contain the newly created package
+     */
+    private def void handlePackageInEChange(GeneralChange change) {
+    	if (change.getEChanges.size == 1) {
+    		val eChange = change.getEChanges.get(0);
+        	if (eChange instanceof InsertRootEObject<?>) {
+	            attachPackageToResource(eChange.getNewValue(), change.getURI());
+        	} else if (eChange instanceof JavaReplaceSingleValuedEAttribute<?,?>) {
+	            prepareRenamePackageInfos(eChange, change.getURI());
+        	} // TODO: package deletion
+        }
+    }
+
+    private def void prepareRenamePackageInfos(JavaReplaceSingleValuedEAttribute<?,?> updateSingleValuedEAttribute,
+            VURI vuri) {
+        if (updateSingleValuedEAttribute.getOldAffectedEObject() instanceof Package
+                && updateSingleValuedEAttribute.getAffectedEObject() instanceof Package) {
+            val Package oldPackage = updateSingleValuedEAttribute.getOldAffectedEObject() as Package;
+            val Package newPackage = updateSingleValuedEAttribute.getAffectedEObject() as Package;
+            this.attachPackageToResource(oldPackage, vuri);
+            var String newVURIKey = vuri.toString();
+            val String oldPackagePath = oldPackage.getName().replace(".", "/");
+            val String newPackagePath = newPackage.getName().replace(".", "/");
+            newVURIKey = newVURIKey.replace(oldPackagePath, newPackagePath);
+            val VURI newVURI = VURI.getInstance(newVURIKey);
+            this.attachPackageToResource(newPackage, newVURI);
+        }
+    }
+
+    private def void attachPackageToResource(EObject eObject, VURI vuri) {
+        if (eObject instanceof Package) {
+            val Package newPackage = eObject as Package;
+            // attach the package to a resource in order to enable the calculation of
+            // a TUID in the transformations
+            val ResourceSet resourceSet = new ResourceSetImpl();
+            val Resource resource = resourceSet.createResource(vuri.getEMFUri());
+            resource.getContents().add(newPackage);
+        }
+    }
+				
+	override processChange(VitruviusChange change, UserInteracting userInteracting, Blackboard blackboard) {
+		if (change instanceof GeneralChange) {
+			handlePackageInEChange(change);
+		}
+		return #[];
+	}
+	
+	override doesProcess(VitruviusChange change) {
+		return change instanceof GeneralChange;
+	}
+				
+}
---
+++ edu.kit.ipd.sdq.vitruvius.casestudies.pcmjava.preprocessors.java2pcm/src/edu/kit/ipd/sdq/vitruvius/casestudies/pcmjava/preprocessors/java2pcm/code2seff/FunctionClassificationStrategyForPackageMapping.java
@@ -0,0 +1,87 @@
+package edu.kit.ipd.sdq.vitruvius.casestudies.pcmjava.preprocessors.java2pcm.code2seff;
+
+import java.util.Set;
+
+import org.apache.log4j.Logger;
+import org.emftext.language.java.members.ClassMethod;
+import org.emftext.language.java.members.Method;
+import org.palladiosimulator.pcm.repository.BasicComponent;
+import org.palladiosimulator.pcm.repository.OperationSignature;
+import org.somox.gast2seff.visitors.AbstractFunctionClassificationStrategy;
+import org.somox.gast2seff.visitors.MethodCallFinder;
+
+import edu.kit.ipd.sdq.vitruvius.casestudies.pcmjava.seffstatements.code2seff.BasicComponentFinding;
+import edu.kit.ipd.sdq.vitruvius.casestudies.pcmjava.util.java2pcm.JaMoPP2PCMUtils;
+import edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes.CorrespondenceInstance;
+import edu.kit.ipd.sdq.vitruvius.framework.contracts.util.datatypes.CorrespondenceInstanceUtil;
+
+/**
+ * FunctionClassificationStrategy for the simple package mapping Strategy.
+ *
+ * @author langhamm
+ *
+ */
+public class FunctionClassificationStrategyForPackageMapping extends AbstractFunctionClassificationStrategy {
+
+    private static final Logger logger = Logger
+            .getLogger(FunctionClassificationStrategyForPackageMapping.class.getSimpleName());
+
+    private final BasicComponentFinding basicComponentFinding;
+    private final CorrespondenceInstance correspondenceInstance;
+    private final BasicComponent myBasicComponent;
+
+    public FunctionClassificationStrategyForPackageMapping(final BasicComponentFinding basicComponentFinding,
+            final CorrespondenceInstance ci, final BasicComponent myBasicComponent) {
+        super(new MethodCallFinder());
+        this.basicComponentFinding = basicComponentFinding;
+        this.correspondenceInstance = ci;
+        this.myBasicComponent = myBasicComponent;
+    }
+
+    /**
+     * A call is an external call if the call's destination is an interface method that corresponds
+     * to an OperationSignature, or if the calls destination is outside of the own component.
+     */
+    @Override
+    protected boolean isExternalCall(final Method method) {
+        if (!JaMoPP2PCMUtils.normalizeURI(method)) {
+            logger.info("Could not normalize URI for method " + method
+                    + ". Method call is not considered as as external call");
+            return false;
+        }
+        final Set<OperationSignature> correspondingSignatures = CorrespondenceInstanceUtil
+                .getCorrespondingEObjectsByType(this.correspondenceInstance, method, OperationSignature.class);
+        if (null != correspondingSignatures && !correspondingSignatures.isEmpty()) {
+            return true;
+        }
+        if (method instanceof ClassMethod) {
+            final BasicComponent basicComponent = this.basicComponentFinding.findBasicComponentForMethod(method,
+                    this.correspondenceInstance);
+            if (null == basicComponent || basicComponent.getId().equals(this.myBasicComponent.getId())) {
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * The call is a library call if the method that is called does not belong to any
+     * BasicComponent.
+     */
+    @Override
+    protected boolean isLibraryCall(final Method method) {
+        final BasicComponent basicComponentOfMethod = this.basicComponentFinding.findBasicComponentForMethod(method,
+                this.correspondenceInstance);
+        if (null == basicComponentOfMethod) {
+            return true;
+        }
+        if (basicComponentOfMethod.getId().equals(this.myBasicComponent.getId())) {
+            return false;
+        }
+        logger.warn("The destination of a call to the method " + method
+                + " is another component than the source component. This should not happen in isLibraryCall.");
+        return true;
+    }
+
+}
