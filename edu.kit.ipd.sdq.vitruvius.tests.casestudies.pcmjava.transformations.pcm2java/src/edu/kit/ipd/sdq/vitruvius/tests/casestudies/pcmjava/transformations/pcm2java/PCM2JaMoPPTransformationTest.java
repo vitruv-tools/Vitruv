3f60794316d04da15493925d80f95b44c4fbcf05
Heiko Klare <heiko.klare@kit.edu>
2016-08-18 08:38:07 +0000

    Correct package naming in PCM2Java transformations.

--- edu.kit.ipd.sdq.vitruvius.tests.casestudies.pcmjava.transformations.pcm2java/src/edu/kit/ipd/sdq/vitruvius/tests/casestudies/pcmjava/transformations/pcm2java/PCM2JaMoPPTransformationTest.java
+++ edu.kit.ipd.sdq.vitruvius.tests.casestudies.pcmjava.transformations.pcm2java/src/edu/kit/ipd/sdq/vitruvius/tests/casestudies/pcmjava/transformations/pcm2java/PCM2JaMoPPTransformationTest.java
@@ -1,4 +1,4 @@
-package edu.kit.ipd.sdq.vitruvius.tests.casestudies.pcmjava.transformations.pcm2jamopp;
+package edu.kit.ipd.sdq.vitruvius.tests.casestudies.pcmjava.transformations.pcm2java;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
