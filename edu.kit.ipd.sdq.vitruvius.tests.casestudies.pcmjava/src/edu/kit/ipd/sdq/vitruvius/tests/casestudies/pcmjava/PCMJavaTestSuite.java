ec02febfb80cde4ab325b34ba5faee868578f2ac
Heiko Klare <heiko.klare@kit.edu>
2016-08-18 15:25:01 +0000

    Make PCM-Java tests refer to the general package mapping test suite instead of the individual ones.

--- edu.kit.ipd.sdq.vitruvius.tests.casestudies.pcmjava/src/edu/kit/ipd/sdq/vitruvius/tests/casestudies/pcmjava/PCMJavaTestSuite.java
+++ edu.kit.ipd.sdq.vitruvius.tests.casestudies.pcmjava/src/edu/kit/ipd/sdq/vitruvius/tests/casestudies/pcmjava/PCMJavaTestSuite.java
@@ -5,17 +5,14 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import edu.kit.ipd.sdq.vitruvius.tests.casestudies.pcmjava.jamoppuidcalculatorandresolver.JaMoPPTUIDCalculatorAndResolverTestSuite;
-import edu.kit.ipd.sdq.vitruvius.tests.casestudies.pcmjava.seffstatements.PCMJavaSeffstatementsTestSuite;
 import edu.kit.ipd.sdq.vitruvius.tests.casestudies.pcmjava.transformations.PCMJavaTransformationsTestSuite;
-import edu.kit.ipd.sdq.vitruvius.tests.casestudies.pcmjava.transformations.packagemapping.pcm2java.PCM2JavaTestSuite;
-import edu.kit.ipd.sdq.vitruvius.tests.casestudies.pcmjava.transformations.packagemapping.java2pcm.Java2PCMTestSuite;
+import edu.kit.ipd.sdq.vitruvius.tests.casestudies.pcmjava.transformations.packagemapping.PCMJavaPackageMappingTestSuite;
 import edu.kit.ipd.sdq.vitruvius.tests.framework.run.SynchronisationTest.SynchronizationTestSuite;
 
 @RunWith(Suite.class)
 
 @SuiteClasses({ JaMoPPTUIDCalculatorAndResolverTestSuite.class, PCMJavaTransformationsTestSuite.class,
-	SynchronizationTestSuite.class, PCM2JavaTestSuite.class, Java2PCMTestSuite.class,
-	PCMJavaSeffstatementsTestSuite.class })
+	SynchronizationTestSuite.class, PCMJavaPackageMappingTestSuite.class })
 public class PCMJavaTestSuite {
 
 }
