081b29ee02804e1792d51318c4ca98484310fbcf
Heiko Klare <heiko.klare@kit.edu>
2016-08-29 13:53:42 +0000

    Remove unused contracts.

--- edu.kit.ipd.sdq.vitruvius.framework.contracts/src/edu/kit/ipd/sdq/vitruvius/framework/contracts/datatypes/IntegrationInfo.java
+++
@@ -1,9 +0,0 @@
-package edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes;
-
-import java.io.Serializable;
-
-public interface IntegrationInfo extends Serializable {
-
-	boolean isCreatedByIntegration();
-
-}
