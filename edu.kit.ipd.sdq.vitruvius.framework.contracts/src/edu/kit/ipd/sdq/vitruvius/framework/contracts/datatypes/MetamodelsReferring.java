958b9a8d423dfbc1b81d33cfc2f064916f8c9663
Heiko Klare <heiko.klare@kit.edu>
2016-08-30 16:03:31 +0000

    Remove unused interface.

--- edu.kit.ipd.sdq.vitruvius.framework.contracts/src/edu/kit/ipd/sdq/vitruvius/framework/contracts/datatypes/MetamodelsReferring.java
+++
@@ -1,8 +0,0 @@
-package edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes;
-
-import edu.kit.ipd.sdq.vitruvius.framework.util.datatypes.VURI;
-
-public interface MetamodelsReferring {
-
-    VURI[] getMetamodelURIs();
-}
