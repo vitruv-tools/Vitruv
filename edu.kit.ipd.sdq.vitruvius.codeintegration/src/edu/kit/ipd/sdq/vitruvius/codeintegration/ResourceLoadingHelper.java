b08b69456b88fb64bb9199d3d07ee554f0ba6563
Heiko Klare <heiko.klare@kit.edu>
2016-09-05 13:01:53 +0000

    Remove duplicated utility class.

--- edu.kit.ipd.sdq.vitruvius.codeintegration/src/edu/kit/ipd/sdq/vitruvius/codeintegration/ResourceLoadingHelper.java
+++ edu.kit.ipd.sdq.vitruvius.codeintegration/src/edu/kit/ipd/sdq/vitruvius/codeintegration/ResourceLoadingHelper.java
@@ -14,6 +14,8 @@ import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.somox.sourcecodedecorator.SourcecodedecoratorPackage;
 import org.splevo.jamopp.extraction.JaMoPPSoftwareModelExtractor;
 
+import edu.kit.ipd.sdq.vitruvius.domains.pcm.util.RepositoryModelLoader;
+
 /**
  * Helper class for loading {@link Resource}s.
  *
