e152c6fd024fb73700b9835c2147ac53d6c96a61
Heiko Klare <klare.heiko@gmail.com>
2016-10-25 19:05:14 +0200

    Update reaction specifications according to metaclass addressing change.

---
+++ dsls/tools.vitruv.dsls.response.tests/src-gen/mir/reactions/reactionsAllElementTypesToAllElementTypes/simpleChangesTests/DeletedNonRootEObjectInListReaction.java
@@ -0,0 +1,69 @@
+package mir.reactions.reactionsAllElementTypesToAllElementTypes.simpleChangesTests;
+
+import allElementTypes.NonRoot;
+import allElementTypes.Root;
+import mir.routines.simpleChangesTests.RoutinesFacade;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.xtext.xbase.lib.Extension;
+import tools.vitruv.extensions.dslsruntime.response.AbstractRepairRoutineRealization;
+import tools.vitruv.extensions.dslsruntime.response.AbstractResponseRealization;
+import tools.vitruv.extensions.dslsruntime.response.ResponseExecutionState;
+import tools.vitruv.extensions.dslsruntime.response.structure.CallHierarchyHaving;
+import tools.vitruv.framework.change.echange.EChange;
+import tools.vitruv.framework.change.echange.feature.reference.RemoveEReference;
+import tools.vitruv.framework.userinteraction.UserInteracting;
+
+@SuppressWarnings("all")
+class DeletedNonRootEObjectInListReaction extends AbstractResponseRealization {
+  public DeletedNonRootEObjectInListReaction(final UserInteracting userInteracting) {
+    super(userInteracting);
+  }
+  
+  public static Class<? extends EChange> getExpectedChangeType() {
+    return RemoveEReference.class;
+  }
+  
+  private boolean checkChangeProperties(final RemoveEReference<Root, NonRoot> change) {
+    EObject changedElement = change.getAffectedEObject();
+    // Check model element type
+    if (!(changedElement instanceof Root)) {
+    	return false;
+    }
+    
+    // Check feature
+    if (!change.getAffectedFeature().getName().equals("multiValuedContainmentEReference")) {
+    	return false;
+    }
+    return true;
+  }
+  
+  public boolean checkPrecondition(final EChange change) {
+    if (!(change instanceof RemoveEReference<?, ?>)) {
+    	return false;
+    }
+    RemoveEReference typedChange = (RemoveEReference)change;
+    if (!checkChangeProperties(typedChange)) {
+    	return false;
+    }
+    getLogger().debug("Passed precondition check of response " + this.getClass().getName());
+    return true;
+  }
+  
+  public void executeResponse(final EChange change) {
+    RemoveEReference<Root, NonRoot> typedChange = (RemoveEReference<Root, NonRoot>)change;
+    mir.routines.simpleChangesTests.RoutinesFacade routinesFacade = new mir.routines.simpleChangesTests.RoutinesFacade(this.executionState, this);
+    mir.reactions.reactionsAllElementTypesToAllElementTypes.simpleChangesTests.DeletedNonRootEObjectInListReaction.ActionUserExecution userExecution = new mir.reactions.reactionsAllElementTypesToAllElementTypes.simpleChangesTests.DeletedNonRootEObjectInListReaction.ActionUserExecution(this.executionState, this);
+    userExecution.callRoutine1(typedChange, routinesFacade);
+  }
+  
+  private static class ActionUserExecution extends AbstractRepairRoutineRealization.UserExecution {
+    public ActionUserExecution(final ResponseExecutionState responseExecutionState, final CallHierarchyHaving calledBy) {
+      super(responseExecutionState);
+    }
+    
+    public void callRoutine1(final RemoveEReference<Root, NonRoot> change, @Extension final RoutinesFacade _routinesFacade) {
+      NonRoot _oldValue = change.getOldValue();
+      _routinesFacade.removeNonRoot(_oldValue);
+    }
+  }
+}
---
+++ dsls/tools.vitruv.dsls.response.tests/src-gen/mir/routines/simpleChangesTests/ReplaceNonRootIdRoutine.java
@@ -0,0 +1,65 @@
+package mir.routines.simpleChangesTests;
+
+import allElementTypes.NonRoot;
+import java.io.IOException;
+import mir.routines.simpleChangesTests.RoutinesFacade;
+import org.eclipse.emf.ecore.EObject;
+import tools.vitruv.extensions.dslsruntime.response.AbstractRepairRoutineRealization;
+import tools.vitruv.extensions.dslsruntime.response.ResponseExecutionState;
+import tools.vitruv.extensions.dslsruntime.response.structure.CallHierarchyHaving;
+
+@SuppressWarnings("all")
+public class ReplaceNonRootIdRoutine extends AbstractRepairRoutineRealization {
+  private RoutinesFacade actionsFacade;
+  
+  private ReplaceNonRootIdRoutine.ActionUserExecution userExecution;
+  
+  private static class ActionUserExecution extends AbstractRepairRoutineRealization.UserExecution {
+    public ActionUserExecution(final ResponseExecutionState responseExecutionState, final CallHierarchyHaving calledBy) {
+      super(responseExecutionState);
+    }
+    
+    public EObject getCorrepondenceSourceTargetElement(final NonRoot nonRoot, final String value) {
+      return nonRoot;
+    }
+    
+    public EObject getElement1(final NonRoot nonRoot, final String value, final NonRoot targetElement) {
+      return targetElement;
+    }
+    
+    public void update0Element(final NonRoot nonRoot, final String value, final NonRoot targetElement) {
+      targetElement.setId(value);
+    }
+  }
+  
+  public ReplaceNonRootIdRoutine(final ResponseExecutionState responseExecutionState, final CallHierarchyHaving calledBy, final NonRoot nonRoot, final String value) {
+    super(responseExecutionState, calledBy);
+    this.userExecution = new mir.routines.simpleChangesTests.ReplaceNonRootIdRoutine.ActionUserExecution(getExecutionState(), this);
+    this.actionsFacade = new mir.routines.simpleChangesTests.RoutinesFacade(getExecutionState(), this);
+    this.nonRoot = nonRoot;this.value = value;
+  }
+  
+  private NonRoot nonRoot;
+  
+  private String value;
+  
+  protected void executeRoutine() throws IOException {
+    getLogger().debug("Called routine ReplaceNonRootIdRoutine with input:");
+    getLogger().debug("   NonRoot: " + this.nonRoot);
+    getLogger().debug("   String: " + this.value);
+    
+    NonRoot targetElement = getCorrespondingElement(
+    	userExecution.getCorrepondenceSourceTargetElement(nonRoot, value), // correspondence source supplier
+    	NonRoot.class,
+    	(NonRoot _element) -> true, // correspondence precondition checker
+    	null);
+    if (targetElement == null) {
+    	return;
+    }
+    initializeRetrieveElementState(targetElement);
+    // val updatedElement userExecution.getElement1(nonRoot, value, targetElement);
+    userExecution.update0Element(nonRoot, value, targetElement);
+    
+    postprocessElementStates();
+  }
+}
