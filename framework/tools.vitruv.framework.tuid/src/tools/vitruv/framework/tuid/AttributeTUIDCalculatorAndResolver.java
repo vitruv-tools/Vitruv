db9e728aaaee4f2bc0186b8833d0d0f6dddb765a
Heiko Klare <klare.heiko@gmail.com>
2016-11-11 13:14:37 +0100

    Make AttributeTuidCalculator expect a List as the input must be ordered.

--- framework/tools.vitruv.framework.tuid/src/tools/vitruv/framework/tuid/AttributeTUIDCalculatorAndResolver.java
+++ framework/tools.vitruv.framework.tuid/src/tools/vitruv/framework/tuid/AttributeTUIDCalculatorAndResolver.java
@@ -2,7 +2,6 @@ package tools.vitruv.framework.tuid;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 
 import org.eclipse.emf.ecore.EObject;
@@ -25,7 +24,7 @@ public class AttributeTUIDCalculatorAndResolver extends HierarchicalTUIDCalculat
         this.attributeNames = Arrays.asList(attributeNames);
     }
 
-    public AttributeTUIDCalculatorAndResolver(final String tuidPrefix, final Collection<String> attributeNames) {
+    public AttributeTUIDCalculatorAndResolver(final String tuidPrefix, final List<String> attributeNames) {
         super(tuidPrefix);
         this.attributeNames = new ArrayList<String>(attributeNames);
     }
