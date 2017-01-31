b2702e9c533130c5497f0d4016d39703ca0a78cb
Sebastian Faller <=>
2017-01-31 12:50:23 +0100

    Application Selection works and returns it's reuslt to the wizard.

--- bundles/framework/tools.vitruv.framework.vsum.ui/src/tools/vitruv/framework/vsum/ui/ApplicationSelectionPage.java
+++ bundles/framework/tools.vitruv.framework.vsum.ui/src/tools/vitruv/framework/vsum/ui/ApplicationSelectionPage.java
@@ -1,12 +1,7 @@
 package tools.vitruv.framework.vsum.ui;
 
-import org.eclipse.core.resources.IContainer;
-import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExtension;
 import org.eclipse.core.runtime.IExtensionPoint;
 import org.eclipse.core.runtime.IExtensionRegistry;
@@ -15,7 +10,6 @@ import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Label;
@@ -24,23 +18,22 @@ import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.Tree;
 import org.eclipse.swt.widgets.TreeItem;
 
-import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 
 public class ApplicationSelectionPage extends WizardPage {
 
   private static final String PAGENAME = "Vitruvius Project";
   private static final String DESCRIPTION = "Create a new Vitruvius Project.";
 
-  private Text text1;
-
   private Composite container;
+  private Collection<IExtension> selectedApplications;
 
   protected ApplicationSelectionPage() {
     super(PAGENAME);
     setTitle(PAGENAME);
     setDescription(DESCRIPTION);
-    // setControl(text1);
-
+    selectedApplications = new HashSet<>();
   }
 
   @Override
@@ -50,20 +43,47 @@ public class ApplicationSelectionPage extends WizardPage {
     container.setLayout(layout);
     layout.numColumns = 1;
     Label label1 = new Label(container, SWT.NONE);
-    label1.setText("Say hello to Fred");
- 
+    label1.setText("Select your applications.");
+    
+    final Tree tree = new Tree(container, SWT.CHECK | SWT.BORDER | SWT.V_SCROLL | SWT.H_SCROLL);
+    GridData treeGridData = new GridData(GridData.FILL_BOTH);
+    tree.setLayoutData(treeGridData);
+
+
+    IExtensionRegistry reg = Platform.getExtensionRegistry();
+    IExtensionPoint ep = reg.getExtensionPoint("tools.vitruv.framework.vsum.application");
+    IExtension[] extensions = null;
+    if (ep != null) {
+      extensions = ep.getExtensions();
+    }
+    
+    for (IExtension ex : extensions) {
+      TreeItem t = new TreeItem(tree, SWT.CHECK);
+      t.setText(ex.getLabel());
+      t.setData(ex);
+    }
+
+    tree.addListener(SWT.Selection, new Listener() {
+      
+      @Override
+      public void handleEvent(Event event) {
+        if (event.detail == SWT.CHECK){
+          TreeItem item = (TreeItem) event.item;
+          if (item.getChecked()) {
+            selectedApplications.add((IExtension)item.getData());
+          } else {
+            selectedApplications.remove(item.getData());
+          }
+          setPageComplete(!selectedApplications.isEmpty());
+        }
+      }
+    });
+  
     setControl(container);
     setPageComplete(false);
   }
 
-
-
-  public Text getText1() {
-    return text1;
+  public Collection<IExtension> getSelectedApplications() {
+    return selectedApplications;
   }
-
-  private IProject[] getProjects() {
-    return ResourcesPlugin.getWorkspace().getRoot().getProjects();
-  }
-
 }
--- bundles/framework/tools.vitruv.framework.vsum.ui/src/tools/vitruv/framework/vsum/ui/NewVitruvWizard.java
+++ bundles/framework/tools.vitruv.framework.vsum.ui/src/tools/vitruv/framework/vsum/ui/NewVitruvWizard.java
@@ -1,12 +1,17 @@
 package tools.vitruv.framework.vsum.ui;
 
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IExtension;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class NewVitruvWizard extends Wizard implements INewWizard {
-  
+
   private static final String WINDOWTITLE = "New Vitruvius Project";
   protected ProjectNamePage one;
   protected DomainSelectionPage two;
@@ -26,7 +31,7 @@ public class NewVitruvWizard extends Wizard implements INewWizard {
   public String getWindowTitle() {
     return WINDOWTITLE;
   }
-  
+
   @Override
   public void addPages() {
     one = new ProjectNamePage(this);
@@ -36,11 +41,19 @@ public class NewVitruvWizard extends Wizard implements INewWizard {
     addPage(two);
     addPage(three);
   }
-  
+
   @Override
   public boolean performFinish() {
-    System.out.println(one.getEnteredName());
-    System.out.println(two.getCheckedDomains());
+    System.out.println("Name " + one.getEnteredName());
+    HashMap<IProject, HashSet<IExtension>> map = two.getCheckedDomains();
+    for (IProject p : map.keySet()) {
+      for (IExtension ex : map.get(p)) {
+        System.out.println("Ausgewaehlt wurde zu " + p.getName()+": " + ex.getLabel());
+      }
+    }
+    for(IExtension ex : three.getSelectedApplications()){
+      System.out.println("Ausgewaehlte Application: "+ex.getLabel());
+    }
     return true;
   }
 
