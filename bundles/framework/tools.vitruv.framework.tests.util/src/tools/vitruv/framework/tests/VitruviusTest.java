a4675a829fc7863749fa4e0f4c4c59315d0bc5fe
Heiko Klare <heiko.klare@kit.edu>
2017-04-13 14:45:04 +0200

    Perform test project initialization by replaceable functional

--- bundles/framework/tools.vitruv.framework.tests.util/src/tools/vitruv/framework/tests/VitruviusTest.java
+++ bundles/framework/tools.vitruv.framework.tests.util/src/tools/vitruv/framework/tests/VitruviusTest.java
@@ -2,6 +2,8 @@ package tools.vitruv.framework.tests;
 
 import static org.junit.Assert.fail;
 
+import java.util.function.Function;
+
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.junit.Before;
@@ -13,8 +15,9 @@ import tools.vitruv.framework.tests.util.TestUtil;
 import tools.vitruv.framework.tuid.TuidManager;
 
 /**
- * Basic test class for all Vitruvius tests that require a test project within the test workspace. 
- * The class creates a test project for each test case within the workspace of the Eclipse test instance.
+ * Basic test class for all Vitruvius tests that require a test project within
+ * the test workspace. The class creates a test project for each test case
+ * within the workspace of the Eclipse test instance.
  * 
  * @author Heiko Klare
  */
@@ -23,6 +26,7 @@ public class VitruviusTest {
 	public TestName testName = new TestName();
 
 	private IProject currentTestProject;
+	private Function<String, IProject> testProjectCreator;
 
 	@BeforeClass
 	public static void setUpAllTests() {
@@ -30,18 +34,30 @@ public class VitruviusTest {
 	}
 
 	public VitruviusTest() {
-		super();
+		this.testProjectCreator = VitruviusTest::initializeTestProject;
 	}
 
 	/**
-	 * Initializes each test case and creates the test project in the test workspace. 
-	 * When overwriting this method, ensure that the super method gets called.  
+	 * Overwrites the default creator for initializing the test project with the
+	 * given one
+	 * 
+	 * @param testProjectCreator
+	 *            - the new test project creator
+	 */
+	protected void setTestProjectCreator(Function<String, IProject> testProjectCreator) {
+		this.testProjectCreator = testProjectCreator;
+	}
+
+	/**
+	 * Initializes each test case and creates the test project in the test
+	 * workspace. When overwriting this method, ensure that the super method
+	 * gets called.
 	 */
 	@Before
 	public void beforeTest() {
 		TuidManager.getInstance().reinitialize();
 		String testMethodName = testName.getMethodName();
-		this.currentTestProject = initializeTestProject(testMethodName);
+		this.currentTestProject = testProjectCreator.apply(testMethodName);
 	}
 
 	/**
@@ -52,7 +68,7 @@ public class VitruviusTest {
 	 *            - the name of the test project
 	 * @return the created test project
 	 */
-	protected IProject initializeTestProject(final String testName) {
+	private static IProject initializeTestProject(final String testName) {
 		IProject testProject = null;
 		try {
 			testProject = TestUtil.createProject(testName);
