a52365fc3b981c820ea5eaa52f9948b5276880af
Patrick Stoeckle <p.stoeckle@gmx.net>
2017-04-24 23:37:35 +0200

    Xtend in Test project

---
+++ tests/framework/tools.vitruv.framework.versioning.tests/src/tools/vitruv/framework/tests/versioning/AllVersioningTests.java
@@ -0,0 +1,14 @@
+package tools.vitruv.framework.tests.versioning;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ 
+	ExampleTest.class 
+	})
+
+public class AllVersioningTests {
+
+}
---
+++ tests/framework/tools.vitruv.framework.versioning.tests/src/tools/vitruv/framework/tests/versioning/ExampleTest.xtend
@@ -0,0 +1,12 @@
+package tools.vitruv.framework.tests.versioning
+
+import org.junit.Test
+import org.junit.Assert
+
+
+class ExampleTest {
+	@Test
+	def public test1() {
+		Assert.assertFalse(false);
+	}	
+}
