81d6cdd0f59dad26cf7fb24b3d74a05a3c5c139c
Heiko Klare <heiko.klare@kit.edu>
2017-05-24 10:34:16 +0200

    Correct create father reaction

--- bundles/framework/tools.vitruv.applications.familiespersons.families2persons/src-gen/mir/reactions/reactionsFamiliesToPersons/familiesToPersons/CreatedFatherReaction.java
+++ bundles/framework/tools.vitruv.applications.familiespersons.families2persons/src-gen/mir/reactions/reactionsFamiliesToPersons/familiesToPersons/CreatedFatherReaction.java
@@ -10,8 +10,7 @@ import tools.vitruv.extensions.dslsruntime.reactions.AbstractRepairRoutineRealiz
 import tools.vitruv.extensions.dslsruntime.reactions.ReactionExecutionState;
 import tools.vitruv.extensions.dslsruntime.reactions.structure.CallHierarchyHaving;
 import tools.vitruv.framework.change.echange.EChange;
-import tools.vitruv.framework.change.echange.compound.CreateAndInsertNonRoot;
-import tools.vitruv.framework.change.echange.feature.reference.InsertEReference;
+import tools.vitruv.framework.change.echange.feature.reference.ReplaceSingleValuedEReference;
 import tools.vitruv.framework.userinteraction.UserInteracting;
 
 @SuppressWarnings("all")
@@ -21,35 +20,39 @@ class CreatedFatherReaction extends AbstractReactionRealization {
   }
   
   public void executeReaction(final EChange change) {
-    InsertEReference<Family, Member> typedChange = ((CreateAndInsertNonRoot<Family, Member>)change).getInsertChange();
+    ReplaceSingleValuedEReference<Family, Member> typedChange = (ReplaceSingleValuedEReference<Family, Member>)change;
     Family affectedEObject = typedChange.getAffectedEObject();
     EReference affectedFeature = typedChange.getAffectedFeature();
+    Member oldValue = typedChange.getOldValue();
     Member newValue = typedChange.getNewValue();
     mir.routines.familiesToPersons.RoutinesFacade routinesFacade = new mir.routines.familiesToPersons.RoutinesFacade(this.executionState, this);
     mir.reactions.reactionsFamiliesToPersons.familiesToPersons.CreatedFatherReaction.ActionUserExecution userExecution = new mir.reactions.reactionsFamiliesToPersons.familiesToPersons.CreatedFatherReaction.ActionUserExecution(this.executionState, this);
-    userExecution.callRoutine1(affectedEObject, affectedFeature, newValue, routinesFacade);
+    userExecution.callRoutine1(affectedEObject, affectedFeature, oldValue, newValue, routinesFacade);
   }
   
   public static Class<? extends EChange> getExpectedChangeType() {
-    return CreateAndInsertNonRoot.class;
+    return ReplaceSingleValuedEReference.class;
   }
   
   private boolean checkChangeProperties(final EChange change) {
-    InsertEReference<Family, Member> relevantChange = ((CreateAndInsertNonRoot<Family, Member>)change).getInsertChange();
+    ReplaceSingleValuedEReference<Family, Member> relevantChange = (ReplaceSingleValuedEReference<Family, Member>)change;
     if (!(relevantChange.getAffectedEObject() instanceof Family)) {
     	return false;
     }
     if (!relevantChange.getAffectedFeature().getName().equals("father")) {
     	return false;
     }
-    if (!(relevantChange.getNewValue() instanceof Member)) {
+    if (relevantChange.isFromNonDefaultValue() && !(relevantChange.getOldValue() instanceof Member)) {
+    	return false;
+    }
+    if (relevantChange.isToNonDefaultValue() && !(relevantChange.getNewValue() instanceof Member)) {
     	return false;
     }
     return true;
   }
   
   public boolean checkPrecondition(final EChange change) {
-    if (!(change instanceof CreateAndInsertNonRoot)) {
+    if (!(change instanceof ReplaceSingleValuedEReference)) {
     	return false;
     }
     getLogger().debug("Passed change type check of reaction " + this.getClass().getName());
@@ -66,7 +69,7 @@ class CreatedFatherReaction extends AbstractReactionRealization {
       super(reactionExecutionState);
     }
     
-    public void callRoutine1(final Family affectedEObject, final EReference affectedFeature, final Member newValue, @Extension final RoutinesFacade _routinesFacade) {
+    public void callRoutine1(final Family affectedEObject, final EReference affectedFeature, final Member oldValue, final Member newValue, @Extension final RoutinesFacade _routinesFacade) {
       _routinesFacade.createFather(newValue);
     }
   }
