d4ec18394efe23612cfd7d03e64bb1a24f5ea44c
Heiko Klare <heiko.klare@kit.edu>
2017-09-11 17:24:24 +0200

    Finally disable debug output in EMF monitorededitor tests
    Finally fixes #160

--- tests/extensions/emfdomain/tools.vitruv.extensions.emf.monitorededitor.tests/src/tools/vitruv/domains/emf/monitorededitor/test/utils/BasicTestCase.java
+++ tests/extensions/emfdomain/tools.vitruv.extensions.emf.monitorededitor.tests/src/tools/vitruv/domains/emf/monitorededitor/test/utils/BasicTestCase.java
@@ -16,6 +16,8 @@ import java.net.URL;
 import org.apache.log4j.Logger;
 import org.eclipse.emf.common.util.URI;
 
+import tools.vitruv.framework.tests.util.TestUtil;
+
 public class BasicTestCase {
     public static final String PLUGIN_NAME = "tools.vitruv.extensions.emf.monitorededitor.tests";
 
@@ -27,6 +29,7 @@ public class BasicTestCase {
             if (!hasLoggerBeenInitialized) {
                 org.apache.log4j.BasicConfigurator.configure();
                 hasLoggerBeenInitialized = true;
+                TestUtil.initializeLogger();
             }
         }
     }
