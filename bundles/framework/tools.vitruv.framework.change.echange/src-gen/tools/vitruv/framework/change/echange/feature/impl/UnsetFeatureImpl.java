760fe2493b0ddd8f3b9e775696253cfa838d22a4
Heiko Klare <heiko.klare@kit.edu>
2017-09-22 18:51:29 -0500

    Add missing generated EChange files

---
+++ bundles/framework/tools.vitruv.framework.change.echange/src-gen/tools/vitruv/framework/change/echange/feature/impl/UnsetFeatureImpl.java
@@ -0,0 +1,39 @@
+/**
+ */
+package tools.vitruv.framework.change.echange.feature.impl;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+
+import tools.vitruv.framework.change.echange.feature.FeaturePackage;
+import tools.vitruv.framework.change.echange.feature.UnsetFeature;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Unset Feature</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * @generated
+ */
+public class UnsetFeatureImpl<A extends EObject, F extends EStructuralFeature> extends FeatureEChangeImpl<A, F> implements UnsetFeature<A, F> {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected UnsetFeatureImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return FeaturePackage.Literals.UNSET_FEATURE;
+	}
+
+} //UnsetFeatureImpl
