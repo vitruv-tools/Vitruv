141ddb526b71e3e0ce941bf4c0d1b63cb105c0e7
Heiko Klare <heiko.klare@kit.edu>
2019-11-21 20:17:34 +0100

    Adapt naming of address-recipients mapping test project

---
+++ tests/dsls/tools.vitruv.dsls.mappings.addressesXrecipients.tests/src/tools/vitruv/dsls/mappings/addressesXrecipients/tests/AddressesXRecipientsTestSuite.java
@@ -0,0 +1,14 @@
+package tools.vitruv.dsls.mappings.addressesXrecipients.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import tools.vitruv.dsls.mappings.addressesXrecipients.tests.AddressesXRecipientsL2RTest;
+import tools.vitruv.dsls.mappings.addressesXrecipients.tests.AddressesXRecipientsR2LTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({AddressesXRecipientsL2RTest.class, AddressesXRecipientsR2LTest.class})
+public class AddressesXRecipientsTestSuite {
+
+}
