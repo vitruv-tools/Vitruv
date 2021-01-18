e7769da33315377a96f8412b3164c07789dc5710
Heiko Klare <heiko.klare@kit.edu>
2021-01-18 11:10:35 +0100

    Refactor test domains implementation

--- bundles/testutils/tools.vitruv.testutils.domains/src/tools/vitruv/testutils/domains/AllElementTypesDomain.java
+++ bundles/testutils/tools.vitruv.testutils.domains/src/tools/vitruv/testutils/domains/AllElementTypesDomain.java
@@ -1,22 +1,16 @@
 package tools.vitruv.testutils.domains;
 
-import allElementTypes.AllElementTypesPackage;
-import tools.vitruv.framework.domains.AbstractTuidAwareVitruvDomain;
-import tools.vitruv.framework.tuid.AttributeTuidCalculatorAndResolver;
+import java.util.List;
 
-public final class AllElementTypesDomain extends AbstractTuidAwareVitruvDomain {
+import allElementTypes.AllElementTypesPackage;
+
+public final class AllElementTypesDomain extends VitruvTestDomain {
 	public static final String METAMODEL_NAME = "AllElementTypes";
 	public static final String FILE_EXTENSION = "allElementTypes";
-	
+
 	AllElementTypesDomain() {
-		super(METAMODEL_NAME, AllElementTypesPackage.eINSTANCE, 
-				new AttributeTuidCalculatorAndResolver(AllElementTypesPackage.eNS_URI, AllElementTypesPackage.Literals.IDENTIFIED__ID.getName()), 
+		super(METAMODEL_NAME, AllElementTypesPackage.eINSTANCE, List.of(AllElementTypesPackage.Literals.IDENTIFIED__ID),
 				FILE_EXTENSION);
 	}
 
-	@Override
-	public boolean isUserVisible() {
-		return false;
-	}
-	
 }
