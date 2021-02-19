6ce2798a3044c18f0d2cbc436762f219c755bc17
Heiko Klare <dev@heiko.klare.info>
2021-02-19 17:14:45 +0100

    Cleanup domains and their default implementation

--- bundles/framework/tools.vitruv.framework.domains/src/tools/vitruv/framework/domains/repository/VitruvDomainRepository.java
+++ bundles/framework/tools.vitruv.framework.domains/src/tools/vitruv/framework/domains/repository/VitruvDomainRepository.java
@@ -6,8 +6,11 @@ import tools.vitruv.framework.domains.VitruvDomain;
 import tools.vitruv.framework.util.datatypes.VURI;
 
 public interface VitruvDomainRepository extends Iterable<VitruvDomain> {
-    VitruvDomain getDomain(EObject object);
-    boolean hasDomain(EObject object);
-    VitruvDomain getDomain(VURI mmURI);
-    VitruvDomain getDomain(String fileExtension);
+	boolean hasDomain(EObject object);
+
+	VitruvDomain getDomain(EObject object);
+
+	VitruvDomain getDomain(VURI mmURI);
+
+	VitruvDomain getDomain(String fileExtension);
 }
