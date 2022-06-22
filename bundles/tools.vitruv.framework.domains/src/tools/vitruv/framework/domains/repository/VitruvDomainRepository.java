91c730c49ad75c99e69d962b7130e077a8b72eda
Heiko Klare <dev@heiko.klare.info>
2022-06-22 18:27:35 +0200

    Remove obsolete functionality in domain repository

--- bundles/tools.vitruv.framework.domains/src/tools/vitruv/framework/domains/repository/VitruvDomainRepository.java
+++ bundles/tools.vitruv.framework.domains/src/tools/vitruv/framework/domains/repository/VitruvDomainRepository.java
@@ -4,6 +4,4 @@ import tools.vitruv.framework.domains.VitruvDomain;
 
 public interface VitruvDomainRepository extends Iterable<VitruvDomain> {
 	VitruvDomain getDomainForFileExtension(String fileExtension);
-	
-	VitruvDomain getDomainForNsUri(String nsUri);
 }
