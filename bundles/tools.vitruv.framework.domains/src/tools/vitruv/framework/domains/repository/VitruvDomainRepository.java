5a8ab1f78a7f3e011017629ba238068271aae3b7
Heiko Klare <dev@heiko.klare.info>
2022-06-23 20:51:41 +0200

    Remove domains project

--- bundles/tools.vitruv.framework.domains/src/tools/vitruv/framework/domains/repository/VitruvDomainRepository.java
+++
@@ -1,7 +0,0 @@
-package tools.vitruv.framework.domains.repository;
-
-import tools.vitruv.framework.domains.VitruvDomain;
-
-public interface VitruvDomainRepository extends Iterable<VitruvDomain> {
-	VitruvDomain getDomainForFileExtension(String fileExtension);
-}
