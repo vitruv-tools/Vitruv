a7d43657672ab343614908e4895f7c5423c718a5
Heiko Klare <dev@heiko.klare.info>
2022-08-02 16:55:59 +0200

    Move change visualization project to change repository

--- bundles/tools.vitruv.extensions.changevisualization/src/tools/vitruv/extensions/changevisualization/common/ChangeDataSetGenerationListener.java
+++
@@ -1,5 +0,0 @@
-package tools.vitruv.extensions.changevisualization.common;
-
-public interface ChangeDataSetGenerationListener {
-	public void changeDataSetGenerated(ChangeDataSet changeDataSet);
-}
--- bundles/tools.vitruv.extensions.changevisualization/src/tools/vitruv/extensions/changevisualization/tree/TabHighlighting.java
+++
@@ -1,7 +0,0 @@
-package tools.vitruv.extensions.changevisualization.tree;
-
-public interface TabHighlighting {
-	public void setHighlightID(String highlightID);
-
-	public String getHighlightID();
-}
