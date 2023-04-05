7ffd43fdbb1f15173501ef45f025f7410c8471ec
Thomas Weber <thomas.weber@kit.edu>
2023-04-05 09:25:25 +0200

    Implement code suggestion

--- bundles/tools.vitruv.framework.remote/src/tools/vitruv/framework/remote/common/util/Nothing.java
+++
@@ -1,7 +0,0 @@
-package tools.vitruv.framework.remote.common.util;
-
-/**
- * This class is used to represent a return type of nothing in the context of a REST-call.
- */
-public final class Nothing {
-}
