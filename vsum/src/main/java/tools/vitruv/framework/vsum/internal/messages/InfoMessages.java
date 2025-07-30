08f48bad71488f604727c813530ffba528173e6c
tsotnemikadze <tsotnemikadze@gmail.com>
2025-07-30 15:00:51 +0200

    delete test file

--- vsum/src/main/java/tools/vitruv/framework/vsum/internal/messages/InfoMessages.java
+++ vsum/src/main/java/tools/vitruv/framework/vsum/internal/messages/InfoMessages.java
@@ -5,8 +5,8 @@ public class InfoMessages {
     public static final String FINISH_PROPAGATION = "Finished change propagation";
     public static final String DEBUG_START_RECORDING_VIRTUAL_MODEL = "Start recording virtual model";
     public static final String DEBUG_END_RECORDING_VIRTUAL_MODEL = "End recording virtual model";
-    public static final String DEBUG_SAVING_ALL_MODELS = "Saving all models of model repository for VSUM {}";
     public static final String TRACE_PROPAGATED_CHANGES = "Propagated changes: {}";
+    public static final String DEBUG_SAVING_ALL_MODELS = "Saving all models of model repository for VSUM {}";
     public static final String DEBUG_STARTED_SYNC = "Started synchronizing change: {}";
     public static final String DEBUG_FINISHED_SYNC = "Finished synchronizing change: {}";
 
